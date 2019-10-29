4893374664836e7d3f67726253d6cc527c7c148d
Tim Mendoza <tmendoza@twilio.com>
2019-10-29 10:03:22 -0600

    Fix useRoom tests

--- src/hooks/context/useRoom.tsx
+++ src/hooks/context/useRoom.tsx
@@ -1,5 +1,5 @@
 import EventEmitter from 'events';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Video, { ConnectOptions, LocalTrack, Room } from 'twilio-video';
 
 export default function useRoom(
@@ -9,26 +9,28 @@ export default function useRoom(
 ) {
   const [room, setRoom] = useState<Room>(new EventEmitter() as Room);
   const [isConnecting, setIsConnecting] = useState(false);
+  const disconnectHandlerRef = useRef<() => void>(() => {});
 
   useEffect(() => {
-    let handleBeforeunload: () => void;
-
     if (token && room.state !== 'connected' && !isConnecting) {
       setIsConnecting(true);
       Video.connect(token, { tracks: localTracks, ...options }).then(room => {
         setRoom(room);
-        handleBeforeunload = () => room.disconnect();
+        disconnectHandlerRef.current = () => room.disconnect();
         // @ts-ignore
         window.room = room;
         setIsConnecting(false);
-        window.addEventListener('beforeunload', handleBeforeunload);
+        window.addEventListener('beforeunload', disconnectHandlerRef.current);
       });
     }
 
     return () => {
       if (room.state === 'connected' && !isConnecting) {
         room.disconnect();
-        window.removeEventListener('beforeunload', handleBeforeunload);
+        window.removeEventListener(
+          'beforeunload',
+          disconnectHandlerRef.current
+        );
       }
     };
   }, [token, options, room, localTracks, setIsConnecting, isConnecting]);
