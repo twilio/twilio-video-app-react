914863d31cf15a2d2dcadd920d9e10453aff2eaa
Tim Mendoza <tmendoza@twilio.com>
2019-10-30 15:04:58 -0600

    Add muted audio indicator to ParticipantStrip

--- src/components/ParticipantInfo/ParticipantInfo.tsx
+++ src/components/ParticipantInfo/ParticipantInfo.tsx
@@ -3,6 +3,9 @@ import { styled } from '@material-ui/core/styles';
 import { LocalParticipant, RemoteParticipant } from 'twilio-video';
 import NetworkQualityLevel from '../NewtorkQualityLevel/NetworkQualityLevel';
 import useParticipantNetworkQualityLevel from '../../hooks/useParticipantNetworkQualityLevel/useParticipantNetworkQualityLevel';
+import usePublications from '../../hooks/usePublications/usePublications';
+import usePublicationIsTrackEnabled from '../../hooks/usePublicationIsTrackEnabled/usePublicationIsTrackEnabled';
+import MicOff from '@material-ui/icons/MicOff';
 
 const Container = styled('div')({
   position: 'relative',
@@ -12,7 +15,9 @@ const InfoContainer = styled('div')({
   position: 'absolute',
   zIndex: 1,
   display: 'flex',
+  flexDirection: 'column',
   justifyContent: 'space-between',
+  height: '100%',
   padding: '0.3em',
   width: '100%',
 });
@@ -21,6 +26,11 @@ const Identity = styled('h4')({
   background: 'rgba(0, 0, 0, 0.7)',
   padding: '0.1em 0.3em',
   margin: 0,
+});
+
+const InfoRow = styled('div')({
+  display: 'flex',
+  justifyContent: 'space-between',
 });
 
 interface ParticipantInfoProps {
@@ -33,12 +43,19 @@ export default function ParticipantInfo({
   children,
 }: ParticipantInfoProps) {
   const networkQualityLevel = useParticipantNetworkQualityLevel(participant);
+  const publications = usePublications(participant);
+  const audioIsEnabled = usePublicationIsTrackEnabled(
+    publications.find(p => p.trackName === 'microphone')
+  );
 
   return (
     <Container>
       <InfoContainer>
-        <Identity>{participant.identity}</Identity>
-        <NetworkQualityLevel qualityLevel={networkQualityLevel} />
+        <InfoRow>
+          <Identity>{participant.identity}</Identity>
+          <NetworkQualityLevel qualityLevel={networkQualityLevel} />
+        </InfoRow>
+        {!audioIsEnabled && <MicOff />}
       </InfoContainer>
       {children}
     </Container>
