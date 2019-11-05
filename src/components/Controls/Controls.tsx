b4fff6300eda46ae94c18712cc5645f642efe50f
Tim Mendoza <tmendoza@twilio.com>
2019-11-05 15:58:37 -0700

    Update tests for controls component

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -17,7 +17,7 @@ import useRoomState from '../../hooks/useRoomState/useRoomState';
 
 import { useDispatch } from 'react-redux';
 import { receiveToken } from '../../store/main/main';
-import useScreenShare from '../../hooks/useScreenShare/useScreenShare';
+import useScreenShareToggle from '../../hooks/useScreenShareToggle/useScreenShareToggle';
 import useScreenShareParticipant from '../../hooks/useScreenShareParticipant/useScreenShareParticipant';
 import { useVideoContext } from '../../hooks/context';
 
@@ -44,7 +44,7 @@ export default function Controls() {
   const dispatch = useDispatch();
   const [isAudioEnabled, toggleAudioEnabled] = useLocalAudioToggle();
   const [isVideoEnabled, toggleVideoEnabled] = useLocalVideoToggle();
-  const [isScreenShared, toggleScreenShare] = useScreenShare();
+  const [isScreenShared, toggleScreenShare] = useScreenShareToggle();
   const roomState = useRoomState();
   const screenShareParticipant = useScreenShareParticipant();
   const { room } = useVideoContext();
@@ -72,15 +72,17 @@ export default function Controls() {
       </Tooltip>
       {roomState === 'connected' && (
         <>
-          <Tooltip
-            title={isScreenShared ? 'Stop Screen Sharing' : 'Share Screen'}
-            placement="top"
-            PopperProps={{ disablePortal: true }}
-          >
-            <Fab className={classes.fab} onClick={toggleScreenShare} disabled={disableScreenShareButton}>
-              {isScreenShared ? <StopScreenShare /> : <ScreenShare />}
-            </Fab>
-          </Tooltip>
+          {navigator.mediaDevices.getDisplayMedia && (
+            <Tooltip
+              title={isScreenShared ? 'Stop Screen Sharing' : 'Share Screen'}
+              placement="top"
+              PopperProps={{ disablePortal: true }}
+            >
+              <Fab className={classes.fab} onClick={toggleScreenShare} disabled={disableScreenShareButton}>
+                {isScreenShared ? <StopScreenShare /> : <ScreenShare />}
+              </Fab>
+            </Tooltip>
+          )}
           <Tooltip
             title={'End Call'}
             onClick={() => dispatch(receiveToken(''))}
