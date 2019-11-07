0416b15fac5887d76c11911b3d05dce921075e2d
Tim Mendoza <tmendoza@twilio.com>
2019-11-07 10:58:40 -0700

    Add basic auth to token server

--- server.js
+++ server.js
@@ -11,14 +11,30 @@ const twilioApiSecret = process.env.API_SECRET;
 
 app.use(bodyParser.json());
 
+if (process.env.USE_BASIC_AUTH === 'true') {
+  app.use(cookieParser());
+  app.use((req, res, next) => {
+    const USER_NAME = process.env.BASIC_AUTH_USERNAME;
+    const PASSWORD = process.env.BASIC_AUTH_PASSWORD;
+
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+    const [login, password] = new Buffer.from(b64auth, 'base64').toString().split(':');
+    const isPasswordValid = login && password && login === USER_NAME && password === PASSWORD;
+    const isCookieValid = req.cookies.auth === b64auth; 
+
+    if (isCookieValid || isPasswordValid) {
+      passwordIsValid && res.cookie('auth', b64auth, { expires: new Date(Date.now() + 900000) });
+      return next();
+    }
+
+    res.set('WWW-Authenticate', 'Basic realm="Restricted"');
+    res.status(401).send('Authentication required.');
+  });
+}
+
 app.post('/token', (req, res) => {
   const { name, room } = req.body;
-  const token = new AccessToken(
-    twilioAccountSid,
-    twilioApiKey,
-    twilioApiSecret,
-    { ttl: MAX_ALLOWED_SESSION_DURATION }
-  );
+  const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret, { ttl: MAX_ALLOWED_SESSION_DURATION });
   token.identity = name;
   const videoGrant = new VideoGrant({ room });
   token.addGrant(videoGrant);
