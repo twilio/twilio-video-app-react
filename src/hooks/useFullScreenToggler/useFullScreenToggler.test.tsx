6c51b1e67e65940fed000830e9ce3becef6e6220
Ricardo Recaredo <rrecaredo@twilio.com>
2019-11-07 04:49:09 -0800

    PR feedback

--- src/hooks/useFullScreenToggler/useFullScreenToggler.test.tsx
+++ src/hooks/useFullScreenToggler/useFullScreenToggler.test.tsx
@@ -2,29 +2,19 @@ import { act, renderHook } from '@testing-library/react-hooks';
 import useFullScreenToggler from './useFullScreenToggler';
 import fscreen from 'fscreen';
 
-jest.mock('fscreen', () => {
-  const {
-    default: { addEventListener, removeEventListener },
-  } = jest.requireActual('fscreen');
-  return {
-    addEventListener,
-    removeEventListener,
-    exitFullscreen: jest.fn(),
-    requestFullscreen: jest.fn(),
-  };
-});
-
-const dispatchFullScreenChangeEvent = (fullscreenElement: string | null) => {
-  // @ts-ignore readonly property
-  fscreen.fullscreenElement = fullscreenElement;
-  const enterFullScreenEvent = new CustomEvent('fullscreenchange', { bubbles: true });
-  document.dispatchEvent(enterFullScreenEvent);
-};
+jest.mock('fscreen', () => ({
+  addEventListener: jest.fn(),
+  removeEventListener: jest.fn(),
+  exitFullscreen: jest.fn(),
+  requestFullscreen: jest.fn(),
+  fullscreenElement: null,
+}));
 
 describe('the useFullScreenToggler hook', () => {
   beforeEach(() => {
     // @ts-ignore readonly property
     fscreen.fullscreenElement = null;
+    jest.clearAllMocks();
   });
 
   it('should return a binary array with a getter an the toggler', () => {
@@ -41,6 +31,7 @@ describe('the useFullScreenToggler hook', () => {
     });
     expect(fscreen.requestFullscreen).toHaveBeenCalledTimes(1);
   });
+
   it('should exit full screen when it gets toggled and it is activated', () => {
     // @ts-ignore readonly property
     fscreen.fullscreenElement = 'Something';
@@ -53,21 +44,33 @@ describe('the useFullScreenToggler hook', () => {
     });
     expect(fscreen.exitFullscreen).toHaveBeenCalledTimes(1);
   });
+
   it('should react to fullscreenchange event and update isFullScreen accordingly', async () => {
     const { result } = renderHook(useFullScreenToggler);
+    expect(fscreen.addEventListener).toHaveBeenCalledWith('fullscreenchange', expect.any(Function));
+    const mockSetIsFullscreen = (fscreen.addEventListener as jest.Mock<any>).mock.calls[0][1];
 
     act(() => {
-      dispatchFullScreenChangeEvent('element');
+      // @ts-ignore readonly property
+      fscreen.fullscreenElement = true;
+      mockSetIsFullscreen();
     });
 
-    let [isFullScreen] = result.current;
-    expect(isFullScreen).toBe(true);
+    expect(result.current[0]).toBe(true);
 
     act(() => {
-      dispatchFullScreenChangeEvent(null);
+      // @ts-ignore readonly property
+      fscreen.fullscreenElement = false;
+      mockSetIsFullscreen();
     });
 
-    [isFullScreen] = result.current;
-    expect(isFullScreen).toBe(false);
+    expect(result.current[0]).toBe(false);
+  });
+
+  it('should remove listeners on unmount', () => {
+    const { unmount } = renderHook(useFullScreenToggler);
+    expect(fscreen.addEventListener).toHaveBeenCalledWith('fullscreenchange', expect.any(Function));
+    unmount();
+    expect(fscreen.removeEventListener).toHaveBeenCalledWith('fullscreenchange', expect.any(Function));
   });
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,3 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-// @ts-ignore
-document.fullscreenEnabled = true;
 configure({ adapter: new Adapter() });
