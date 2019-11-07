bab9b26f4d8881c5843143bb1ed93a78462cbbdf
Tim Mendoza <tmendoza@twilio.com>
2019-11-07 11:48:11 -0700

    Remove cookies from server

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const app = require('express')();
 const AccessToken = require('twilio').jwt.AccessToken;
 const VideoGrant = AccessToken.VideoGrant;
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const MAX_ALLOWED_SESSION_DURATION = 14400;
@@ -13,18 +12,14 @@ const twilioApiSecret = process.env.API_SECRET;
 app.use(bodyParser.json());
 
 if (process.env.USE_BASIC_AUTH === 'true') {
-  app.use(cookieParser());
   app.use((req, res, next) => {
     const USER_NAME = process.env.BASIC_AUTH_USERNAME;
     const PASSWORD = process.env.BASIC_AUTH_PASSWORD;
 
     const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
     const [login, password] = new Buffer.from(b64auth, 'base64').toString().split(':');
-    const isPasswordValid = login && password && login === USER_NAME && password === PASSWORD;
-    const isCookieValid = req.cookies.auth === b64auth;
 
-    if (isCookieValid || isPasswordValid) {
-      passwordIsValid && res.cookie('auth', b64auth, { expires: new Date(Date.now() + 900000) });
+    if (login && password && login === USER_NAME && password === PASSWORD) {
       return next();
     }
 
