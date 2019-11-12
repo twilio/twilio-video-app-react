1cb7e9b2b9c81a576324c390ee4cb6feef187556
Tim Mendoza <tmendoza@twilio.com>
2019-11-12 11:16:28 -0700

    Fix tests for useIsTrackSwitchedOff

--- src/hooks/useIsTrackSwitchedOff/useIsTrackSwitchedOff.test.tsx
+++ src/hooks/useIsTrackSwitchedOff/useIsTrackSwitchedOff.test.tsx
@@ -11,7 +11,7 @@ describe('the useIsTrackSwitchedOff hook', () => {
 
   it('should return false when track is undefined', () => {
     const { result } = renderHook(() => useIsTrackSwitchedOff(undefined));
-    expect(result.current).toBe(true);
+    expect(result.current).toBe(false);
   });
 
   it('should return mockTrack.isSwitchedOff by default', () => {
@@ -21,21 +21,23 @@ describe('the useIsTrackSwitchedOff hook', () => {
   });
 
   it('should return respond to "switchedOff" events', async () => {
-    mockTrack.isSwitchedOff = true;
+    mockTrack.isSwitchedOff = false;
     const { result } = renderHook(() => useIsTrackSwitchedOff(mockTrack));
     act(() => {
+      mockTrack.isSwitchedOff = true;
       mockTrack.emit('switchedOff');
     });
-    expect(result.current).toBe(false);
+    expect(result.current).toBe(true);
   });
 
   it('should return respond to "switchedOn" events', async () => {
-    mockTrack.isEnabled = false;
+    mockTrack.isEnabled = true;
     const { result } = renderHook(() => useIsTrackSwitchedOff(mockTrack));
     act(() => {
+      mockTrack.isSwitchedOff = false;
       mockTrack.emit('switchedOn');
     });
-    expect(result.current).toBe(true);
+    expect(result.current).toBe(false);
   });
 
   it('should clean up listeners on unmount', () => {
