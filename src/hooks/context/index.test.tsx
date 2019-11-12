499ad98fee0b65d4b78790571d8afca56d7d6092
Tim Mendoza <tmendoza@twilio.com>
2019-11-12 14:38:48 -0700

    Add sanity check to useVideoContext

--- src/hooks/context/index.test.tsx
+++ src/hooks/context/index.test.tsx
@@ -6,6 +6,7 @@ import { VideoProvider, useVideoContext } from './index';
 
 jest.mock('./useRoom/useRoom', () => jest.fn(() => ({ room: 'mockRoom', isConnecting: false })));
 jest.mock('./useLocalTracks/useLocalTracks', () => jest.fn(() => ['mockTrack']));
+
 describe('the useVideoContext hook', () => {
   it('should correct return the Video Context object', () => {
     const wrapper: React.FC = ({ children }) => (
@@ -23,5 +24,10 @@ describe('the useVideoContext hook', () => {
       dominantSpeaker: true,
     });
     expect(useLocalTracks).toHaveBeenCalled();
+  });
+
+  it('should throw an error if used outside of the VideoProvider', () => {
+    const { result } = renderHook(useVideoContext);
+    expect(result.error.message).toBe('useVideoContext must be used within a VideoProvider');
   });
 });
