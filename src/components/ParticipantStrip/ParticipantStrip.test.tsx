6a5070e7d6d22d803f88a262647e9906f2bcc216
Tim Mendoza <tmendoza@twilio.com>
2019-11-13 18:03:18 -0700

    Update participantStrip tests

--- src/components/ParticipantStrip/ParticipantStrip.test.tsx
+++ src/components/ParticipantStrip/ParticipantStrip.test.tsx
@@ -2,12 +2,17 @@ import EventEmitter from 'events';
 import React from 'react';
 import ParticipantStrip from './ParticipantStrip';
 import { shallow } from 'enzyme';
+import useSelectedParticipant from '../../hooks/context/useSelectedParticipant/useSelectedParticipant';
 import { useVideoContext } from '../../hooks/context';
 
 jest.mock('../../hooks/context');
+jest.mock('../../hooks/context/useSelectedParticipant/useSelectedParticipant');
 const mockedVideoContext = useVideoContext as jest.Mock<any>;
+const mockUseSelectedParticipant = useSelectedParticipant as jest.Mock<any>;
 
 describe('the ParticipantStrip component', () => {
+  mockUseSelectedParticipant.mockImplementation(() => [null, () => {}]);
+
   it('should correctly render ParticipantInfo components', () => {
     const mockRoom: any = new EventEmitter();
     mockRoom.participants = new Map([[0, { sid: 0 }], [1, { sid: 1 }]]);
@@ -15,5 +20,36 @@ describe('the ParticipantStrip component', () => {
     mockedVideoContext.mockImplementation(() => ({ room: mockRoom }));
     const wrapper = shallow(<ParticipantStrip />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should add the isSelected prop to the local participant when it is selected', () => {
+    mockUseSelectedParticipant.mockImplementation(() => ['localParticipant', () => {}]);
+    const mockRoom: any = new EventEmitter();
+    mockRoom.participants = new Map([[0, { sid: 0 }], [1, { sid: 1 }]]);
+    mockRoom.localParticipant = 'localParticipant';
+    mockedVideoContext.mockImplementation(() => ({ room: mockRoom }));
+    const wrapper = shallow(<ParticipantStrip />);
+    expect(
+      wrapper
+        .find('Participant')
+        .at(0)
+        .prop('isSelected')
+    ).toBe(true);
+  });
+
+  it('should add the isSelected prop to the first remote participant when it is selected', () => {
+    const mockParticipant = { sid: 0 };
+    mockUseSelectedParticipant.mockImplementation(() => [mockParticipant, () => {}]);
+    const mockRoom: any = new EventEmitter();
+    mockRoom.participants = new Map([[0, mockParticipant], [1, { sid: 1 }]]);
+    mockRoom.localParticipant = 'localParticipant';
+    mockedVideoContext.mockImplementation(() => ({ room: mockRoom }));
+    const wrapper = shallow(<ParticipantStrip />);
+    expect(
+      wrapper
+        .find('Participant')
+        .at(1)
+        .prop('isSelected')
+    ).toBe(true);
   });
 });
