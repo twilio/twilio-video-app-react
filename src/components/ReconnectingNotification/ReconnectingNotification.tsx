628ec1667cd638c9ebc226456e00d045d4c8a65e
Tim Mendoza <tmendoza@twilio.com>
2019-11-15 14:33:14 -0700

    Improve ReconnectingNotification styles

--- src/components/ReconnectingNotification/ReconnectingNotification.tsx
+++ src/components/ReconnectingNotification/ReconnectingNotification.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import InfoIcon from '@material-ui/icons/Info';
+import { makeStyles } from '@material-ui/core/styles';
 import Snackbar from '@material-ui/core/Snackbar';
 import useRoomState from '../../hooks/useRoomState/useRoomState';
+import { SnackbarContent } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  snackbar: {
+    backgroundColor: '#6db1ff',
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  icon: {
+    marginRight: '0.8em',
+  },
+});
 
 export default function ReconnectingNotification() {
+  const classes = useStyles();
   const roomState = useRoomState();
+
   return (
-    <Snackbar
-      anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-      message="Reconnecting&hellip;"
-      open={roomState === 'reconnecting'}
-    />
+    <Snackbar anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} open={roomState === 'reconnecting'}>
+      <SnackbarContent
+        className={classes.snackbar}
+        message={
+          <span className={classes.message}>
+            <InfoIcon className={classes.icon} />
+            Reconnecting&hellip;
+          </span>
+        }
+      />
+    </Snackbar>
   );
 }
