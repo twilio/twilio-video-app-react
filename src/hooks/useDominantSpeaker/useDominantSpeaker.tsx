128c4dee62086b6a18d42d9841cc45299be51994
timmydoza <tmendoza@twilio.com>
2019-11-15 11:45:21 -0700

    AHOYAPPS-139 Improve behavior of dominant speaker hook (#26)
    * Improve behavior of dominant speaker hook
    
    * Fix issue found by linter

--- src/hooks/useDominantSpeaker/useDominantSpeaker.tsx
+++ src/hooks/useDominantSpeaker/useDominantSpeaker.tsx
@@ -1,14 +1,29 @@
 import { useEffect, useState } from 'react';
 import { useVideoContext } from '../context';
+import { RemoteParticipant } from 'twilio-video';
 
 export default function useDominantSpeaker() {
   const { room } = useVideoContext();
   const [dominantSpeaker, setDominantSpeaker] = useState(room.dominantSpeaker);
 
   useEffect(() => {
-    room.on('dominantSpeakerChanged', setDominantSpeaker);
+    const handleDominantSpeakerChanged = (newDominantSpeaker: RemoteParticipant) => {
+      if (newDominantSpeaker !== null) {
+        setDominantSpeaker(newDominantSpeaker);
+      }
+    };
+
+    const handleParticipantDisconnected = (participant: RemoteParticipant) => {
+      setDominantSpeaker(prevDominantSpeaker => {
+        return prevDominantSpeaker === participant ? null : prevDominantSpeaker;
+      });
+    };
+
+    room.on('dominantSpeakerChanged', handleDominantSpeakerChanged);
+    room.on('participantDisconnected', handleParticipantDisconnected);
     return () => {
-      room.off('dominantSpeakerChanged', setDominantSpeaker);
+      room.off('dominantSpeakerChanged', handleDominantSpeakerChanged);
+      room.off('participantDisconnected', handleParticipantDisconnected);
     };
   }, [room]);
 
