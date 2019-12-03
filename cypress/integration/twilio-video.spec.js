6461906341ef77dd12c70bea4e3d1ba579f727c5
Tim Mendoza <tmendoza@twilio.com>
2019-12-03 13:58:43 -0700

    Add initial batch of tests

---
+++ cypress/integration/twilio-video.spec.js
@@ -0,0 +1,96 @@
+/// <reference types="Cypress" />
+
+// Creates a random string like 'ft68eyjn8i'
+const getRoomName = () =>
+  Math.random()
+    .toString(36)
+    .slice(2); 
+
+context('A video app user', () => {
+  describe('when entering an empty room that one participant will join', () => {
+    const ROOM_NAME = getRoomName();
+
+    before(() => {
+      cy.joinRoom('testuser', ROOM_NAME);
+      cy.task('addParticipant', { name: 'test1', roomName: ROOM_NAME });
+    });
+
+    after(() => {
+      cy.leaveRoom();
+    });
+
+    it('should be inside the correct room', () => {
+      cy.get('header').should('contain', ROOM_NAME);
+      cy.getParticipant('testuser').should('contain', 'testuser');
+    });
+
+    it('should be able to see the other participant', () => {
+      cy.get('[data-cy-main-participant]').should('contain', 'test1');
+      cy.getParticipant('test1').shouldBeSameVideoAs('[data-cy-main-participant]')
+    });
+  });
+
+  describe('when entering an empty room that one participant will join', () => {
+    const ROOM_NAME = getRoomName();
+
+    before(() => {
+      cy.task('addParticipant', { name: 'test1', roomName: ROOM_NAME });
+      cy.joinRoom('testuser', ROOM_NAME);
+    });
+
+    after(() => {
+      cy.leaveRoom();
+    });
+
+    it('should be inside the correct room', () => {
+      cy.get('header').should('contain', ROOM_NAME);
+      cy.getParticipant('testuser').should('contain', 'testuser');
+    });
+
+    it('should be able to see the other participant', () => {
+      cy.get('[data-cy-main-participant]').should('contain', 'test1');
+      cy.getParticipant('test1').shouldBeSameVideoAs('[data-cy-main-participant]')
+    });
+  });
+
+  describe('when entering an empty room that three participants will join', () => {
+    const ROOM_NAME = getRoomName();
+
+    before(() => {
+      cy.joinRoom('testuser', ROOM_NAME);
+      cy.task('addParticipant', { name: 'test1', roomName: ROOM_NAME, color: 'red' });
+      cy.task('addParticipant', { name: 'test2', roomName: ROOM_NAME, color: 'blue' });
+      cy.task('addParticipant', { name: 'test3', roomName: ROOM_NAME, color: 'green' });
+      cy.wait(1000);
+    });
+
+    after(() => {
+      cy.leaveRoom();
+    });
+
+    it('should be able to see the other participant', () => {
+      cy.getParticipant('test1').shouldBeColor('red')
+      cy.getParticipant('test2').shouldBeColor('blue')
+      cy.getParticipant('test3').shouldBeColor('green')
+    });
+  });
+
+  describe('when entering a room with three participants', () => {
+    const ROOM_NAME = getRoomName();
+
+    before(() => {
+      cy.task('addParticipant', { name: 'test1', roomName: ROOM_NAME, color: 'red' });
+      cy.task('addParticipant', { name: 'test2', roomName: ROOM_NAME, color: 'blue' });
+      cy.joinRoom('testuser', ROOM_NAME);
+    });
+
+    after(() => {
+      cy.leaveRoom();
+    });
+
+    it('should be able to see the other participant', () => {
+      cy.getParticipant('test1').shouldBeColor('red')
+      cy.getParticipant('test2').shouldBeColor('blue')
+    });
+  });
+});
