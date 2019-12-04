6ebcfdaa15497795276c492efeefcf1515582f21
Tim Mendoza <tmendoza@twilio.com>
2019-12-03 17:19:40 -0700

    Add assertion for checking participant audio

--- cypress/integration/twilio-video.spec.js
+++ cypress/integration/twilio-video.spec.js
@@ -28,9 +28,13 @@ context('A video app user', () => {
       cy.get('[data-cy-main-participant]').should('contain', 'test1');
       cy.getParticipant('test1').shouldBeSameVideoAs('[data-cy-main-participant]')
     });
+
+    it('should be able to hear the other participant', () => {
+      cy.getParticipant('test1').shouldBeMakingSound()
+    })
   });
 
-  describe('when entering an empty room that one participant will join', () => {
+  describe('when entering a room with one participant', () => {
     const ROOM_NAME = getRoomName();
 
     before(() => {
@@ -42,15 +46,14 @@ context('A video app user', () => {
       cy.leaveRoom();
     });
 
-    it('should be inside the correct room', () => {
-      cy.get('header').should('contain', ROOM_NAME);
-      cy.getParticipant('testuser').should('contain', 'testuser');
-    });
-
     it('should be able to see the other participant', () => {
       cy.get('[data-cy-main-participant]').should('contain', 'test1');
       cy.getParticipant('test1').shouldBeSameVideoAs('[data-cy-main-participant]')
     });
+
+    it('should be able to hear the other participant', () => {
+      cy.getParticipant('test1').shouldBeMakingSound()
+    })
   });
 
   describe('when entering an empty room that three participants will join', () => {
@@ -61,17 +64,22 @@ context('A video app user', () => {
       cy.task('addParticipant', { name: 'test1', roomName: ROOM_NAME, color: 'red' });
       cy.task('addParticipant', { name: 'test2', roomName: ROOM_NAME, color: 'blue' });
       cy.task('addParticipant', { name: 'test3', roomName: ROOM_NAME, color: 'green' });
-      cy.wait(1000);
     });
 
     after(() => {
       cy.leaveRoom();
     });
 
-    it('should be able to see the other participant', () => {
+    it('should be able to see the other participants', () => {
       cy.getParticipant('test1').shouldBeColor('red')
       cy.getParticipant('test2').shouldBeColor('blue')
       cy.getParticipant('test3').shouldBeColor('green')
+    });
+
+    it('should be able to hear the other participants', () => {
+      cy.getParticipant('test1').shouldBeMakingSound()
+      cy.getParticipant('test2').shouldBeMakingSound()
+      cy.getParticipant('test3').shouldBeMakingSound()
     });
   });
 
@@ -81,6 +89,7 @@ context('A video app user', () => {
     before(() => {
       cy.task('addParticipant', { name: 'test1', roomName: ROOM_NAME, color: 'red' });
       cy.task('addParticipant', { name: 'test2', roomName: ROOM_NAME, color: 'blue' });
+      cy.task('addParticipant', { name: 'test3', roomName: ROOM_NAME, color: 'green' });
       cy.joinRoom('testuser', ROOM_NAME);
     });
 
@@ -88,9 +97,16 @@ context('A video app user', () => {
       cy.leaveRoom();
     });
 
-    it('should be able to see the other participant', () => {
+    it('should be able to see the other participants', () => {
       cy.getParticipant('test1').shouldBeColor('red')
       cy.getParticipant('test2').shouldBeColor('blue')
+      cy.getParticipant('test3').shouldBeColor('green')
+    });
+
+    it('should be able to hear the other participants', () => {
+      cy.getParticipant('test1').shouldBeMakingSound()
+      cy.getParticipant('test2').shouldBeMakingSound()
+      cy.getParticipant('test3').shouldBeMakingSound()
     });
   });
 });
