29f0273f25d24cb8e0ce389f7a7c75a2cffe7309
timmydoza <tmendoza@twilio.com>
2019-12-17 09:47:11 -0700

    Show screenshare track when camera is disabled (#36)

--- src/components/MainParticipantInfo/MainParticipantInfo.test.tsx
+++ src/components/MainParticipantInfo/MainParticipantInfo.test.tsx
@@ -11,22 +11,6 @@ jest.mock('../../hooks/usePublications/usePublications');
 const mockUsePublications = usePublications as jest.Mock<any>;
 
 describe('the MainParticipantInfo component', () => {
-  it('should add hideVideoProp to InfoContainer component when video is disabled', () => {
-    mockUsePublications.mockImplementation(() => [{ trackName: 'camera', isTrackEnabled: false }]);
-    const wrapper = shallow(
-      <MainParticipantInfo participant={{ identity: 'mockIdentity' } as any}>mock children</MainParticipantInfo>
-    );
-    expect(wrapper.find(InfoContainer).prop('hideVideo')).toEqual(true);
-  });
-
-  it('should not add hideVideoProp to InfoContainer component when video is enabled', () => {
-    mockUsePublications.mockImplementation(() => [{ trackName: 'camera', isTrackEnabled: true }]);
-    const wrapper = shallow(
-      <MainParticipantInfo participant={{ identity: 'mockIdentity' } as any}>mock children</MainParticipantInfo>
-    );
-    expect(wrapper.find(InfoContainer).prop('hideVideo')).toEqual(false);
-  });
-
   it('should render a VideoCamOff icon when no camera tracks are present', () => {
     mockUsePublications.mockImplementation(() => []);
     const wrapper = shallow(
