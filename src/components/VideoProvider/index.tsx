3db322b9faf27b8f2e5c372070be89cca814d509
Tim Mendoza <tmendoza@twilio.com>
2020-01-03 11:36:05 -0700

    Add a comment

--- src/components/VideoProvider/index.tsx
+++ src/components/VideoProvider/index.tsx
@@ -9,6 +9,13 @@ import useHandleTrackPublicationFailed from './useHandleTrackPublicationFailed/u
 import useLocalTracks from './useLocalTracks/useLocalTracks';
 import useRoom from './useRoom/useRoom';
 
+/*
+ *  The hooks used by the VideoProvider component are different than the hooks found in the 'hooks/' directory. The hooks
+ *  in the 'hooks/' directory can be used anywhere in a video application, and they can be used any number of times.
+ *  the hooks in the 'VideoProvider/' directory are intended to be used by the VideoProvider component only. Using these hooks
+ *  elsewhere in the application may cause problems as these hooks should not be used more than once in an application.
+ */
+
 export interface IVideoContext {
   room: Room;
   localTracks: LocalTrack[];
