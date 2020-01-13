7d15419d6e23cdf0ae4bf71f050205696f818b47
Tim Mendoza <tmendoza@twilio.com>
2020-01-13 10:23:51 -0700

    Expose mockRoom from twilio-video mocks

--- src/__mocks__/twilio-video.ts
+++ src/__mocks__/twilio-video.ts
@@ -3,14 +3,18 @@ import { EventEmitter } from 'events';
 class MockRoom extends EventEmitter {
   state = 'connected';
   disconnect = jest.fn();
+  localParticipant = {
+    publishTrack: jest.fn(),
+  };
 }
 
-class MockTwilioVideo {
-  connect = jest.fn(() => Promise.resolve(new MockRoom()));
-  createLocalAudioTrack = jest.fn(() => Promise.resolve(new EventEmitter()));
-  createLocalVideoTrack = jest.fn(() => Promise.resolve(new EventEmitter()));
-}
+const mockRoom = new MockRoom();
 
-const twilioVideo = new MockTwilioVideo();
+const twilioVideo = {
+  connect: jest.fn(() => Promise.resolve(mockRoom)),
+  createLocalAudioTrack: jest.fn(() => Promise.resolve(new EventEmitter())),
+  createLocalVideoTrack: jest.fn(() => Promise.resolve(new EventEmitter())),
+};
 
+export { mockRoom };
 export default twilioVideo;
