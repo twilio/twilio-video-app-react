f24ba4acd68e5001ea163192b6409ee546427d6e
Tim Mendoza <tmendoza@twilio.com>
2020-01-23 12:34:41 -0700

    Add logout handler to userAvatar component

--- src/components/Menu/UserAvatar/UserAvatar.tsx
+++ src/components/Menu/UserAvatar/UserAvatar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import makeStyles from '@material-ui/styles/makeStyles';
@@ -7,6 +7,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Person from '@material-ui/icons/Person';
 
 import { useAppState } from '../../../state';
+import useVideoContext from '../../../hooks/useVideoContext/useVideoContext';
 
 const useStyles = makeStyles({
   red: {
@@ -25,9 +26,17 @@ export function getInititals(name: string) {
 
 export default function UserAvatar() {
   const classes = useStyles();
-  const { signOut, user } = useAppState();
+  const { user, signOut } = useAppState();
+  const { room, localTracks } = useVideoContext();
   const [menuOpen, setMenuOpen] = useState(false);
   const anchorRef = useRef<HTMLDivElement>(null);
+
+  const handleSignOut = useCallback(() => {
+    signOut().then(() => {
+      room.disconnect?.();
+      (localTracks as any[]).forEach(track => track.stop());
+    });
+  }, [room.disconnect, localTracks, signOut]);
 
   if (!user) {
     return null;
@@ -43,7 +52,8 @@ export default function UserAvatar() {
         )}
       </IconButton>
       <Menu open={menuOpen} onClose={() => setMenuOpen(state => !state)} anchorEl={anchorRef.current}>
-        <MenuItem onClick={signOut}>Logout</MenuItem>
+        <MenuItem disabled>{user.displayName}</MenuItem>
+        <MenuItem onClick={handleSignOut}>Logout</MenuItem>
       </Menu>
     </div>
   );
