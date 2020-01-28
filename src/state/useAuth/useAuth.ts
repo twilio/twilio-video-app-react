500cacb220a79a02d3cb19052dfcfb6c84fd9aff
Tim Mendoza <tmendoza@twilio.com>
2020-01-28 15:46:46 -0700

    Pull firebase config from environment variables

--- src/state/useAuth/useAuth.ts
+++ src/state/useAuth/useAuth.ts
@@ -3,11 +3,11 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD3HQ2F0OO1oQgkHxnHoqLEtUhL7laJcAE',
-  authDomain: 'video-app-79418.firebaseapp.com',
-  databaseURL: 'https://video-app-79418.firebaseio.com',
-  storageBucket: 'video-app-79418.appspot.com',
-  messagingSenderId: '285008367772',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
 };
 
 export default function useAuth(setToken: any) {
