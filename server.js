3c96fd56aa9cde175d31f2eefd77be21f9a03db5
Tim Mendoza <tmendoza@twilio.com>
2020-01-30 16:36:10 -0700

    Modify local token server. Make it similar to video-app-service

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require('path');
 const AccessToken = require('twilio').jwt.AccessToken;
 const VideoGrant = AccessToken.VideoGrant;
-const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const MAX_ALLOWED_SESSION_DURATION = 14400;
@@ -11,19 +10,18 @@ const twilioAccountSid = process.env.ACCOUNT_SID;
 const twilioApiKey = process.env.API_KEY;
 const twilioApiSecret = process.env.API_SECRET;
 
-app.use(bodyParser.json());
-
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'build/index.html')));
-
-app.post('/token', ({body: {name, room}}, res) => {
+app.get('/token', (req, res) => {
+  const {identity, roomName} = req.query
   const token = new AccessToken(twilioAccountSid, twilioApiKey, twilioApiSecret, { ttl: MAX_ALLOWED_SESSION_DURATION });
-  token.identity = name;
-  const videoGrant = new VideoGrant({ room });
+  token.identity = identity;
+  const videoGrant = new VideoGrant({ room: roomName });
   token.addGrant(videoGrant);
-  res.json({ token: token.toJwt() });
-  console.log(`issued token for ${name} in room ${room}`);
+  res.text(token.toJwt());
+  console.log(`issued token for ${identity} in room ${roomName}`);
 });
+
+app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'build/index.html')));
 
 app.listen(8080, () => console.log('token server running on 8080'));
