1d2b3e95c6ae87da1dd4f5faf7aba80ab172a32e
Tim Mendoza <tmendoza@twilio.com>
2020-01-31 12:41:20 -0700

    Update state tests

--- src/state/index.test.tsx
+++ src/state/index.test.tsx
@@ -7,12 +7,6 @@ import AppStateProvider, { useAppState } from './index';
 const wrapper: React.FC = ({ children }) => <AppStateProvider>{children}</AppStateProvider>;
 
 describe('the useAppState hook', () => {
-  it('should set a token', () => {
-    const { result } = renderHook(useAppState, { wrapper });
-    act(() => result.current.setToken('test'));
-    expect(result.current.token).toBe('test');
-  });
-
   it('should set an error', () => {
     const { result } = renderHook(useAppState, { wrapper });
     act(() => result.current.setError(new Error('testError') as TwilioError));
@@ -23,14 +17,8 @@ describe('the useAppState hook', () => {
     // @ts-ignore
     window.fetch = jest.fn(() => Promise.resolve({ json: () => ({ token: 'testToken' }) }));
 
-    const { result, waitForNextUpdate } = renderHook(useAppState, { wrapper });
-    result.current.getToken('testname', 'testroom');
-    await waitForNextUpdate();
-    expect(window.fetch).toHaveBeenCalledWith(
-      '/token',
-      expect.objectContaining({ body: JSON.stringify({ name: 'testname', room: 'testroom' }) })
-    );
-    expect(result.current.token).toBe('testToken');
+    const { result } = renderHook(useAppState, { wrapper });
+    return result.current.getToken('testname', 'testroom').then(token => expect(token).toBe('testToken'));
   });
 
   it('should throw an error if used outside of AppStateProvider', () => {
