dd15d87be6a57c64cdf1608fc426ad61bebbda0d
Tim Mendoza <tmendoza@twilio.com>
2020-01-30 17:09:03 -0700

    Rename an enviroment variable

--- src/state/useAuth/useAuth.ts
+++ src/state/useAuth/useAuth.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 export default function useAuth(setToken: any) {
