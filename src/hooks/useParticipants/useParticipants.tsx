b8f7a1d72cfe067b9117a85cfeb6265e11073477
Tim Mendoza <tmendoza@twilio.com>
2020-02-10 10:47:28 -0700

    Add a comment

--- src/hooks/useParticipants/useParticipants.tsx
+++ src/hooks/useParticipants/useParticipants.tsx
@@ -8,6 +8,9 @@ export default function useParticipants() {
   const dominantSpeaker = useDominantSpeaker();
   const [participants, setParticipants] = useState(Array.from(room.participants.values()));
 
+  // When the dominant speaker changes, they are moved to the front of the participants array.
+  // This means that the most recent dominant speakers will always be near the top of the
+  // ParticipantStrip component.
   useEffect(() => {
     if (dominantSpeaker) {
       setParticipants(prevParticipants => [
