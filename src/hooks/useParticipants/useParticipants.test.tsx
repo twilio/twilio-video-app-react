e66c4f21f21ea4c3ad0cd46ef9c3c6d56356094c
timmydoza <tmendoza@twilio.com>
2020-02-10 11:07:10 -0700

    Reorder participants when the dominant speaker changes (#71)
    * Reorder participants when the dominant speaker changes
    
    * Fix dependency array
    
    * Fix linting issue
    
    * Add a comment

--- src/hooks/useParticipants/useParticipants.test.tsx
+++ src/hooks/useParticipants/useParticipants.test.tsx
@@ -1,10 +1,13 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import EventEmitter from 'events';
+import useDominantSpeaker from '../useDominantSpeaker/useDominantSpeaker';
 import useParticipants from './useParticipants';
 import useVideoContext from '../useVideoContext/useVideoContext';
 
 jest.mock('../useVideoContext/useVideoContext');
+jest.mock('../useDominantSpeaker/useDominantSpeaker');
 
+const mockUseDominantSpeaker = useDominantSpeaker as jest.Mock<any>;
 const mockedVideoContext = useVideoContext as jest.Mock<any>;
 
 describe('the useParticipants hook', () => {
@@ -12,7 +15,10 @@ describe('the useParticipants hook', () => {
 
   beforeEach(() => {
     mockRoom = new EventEmitter();
-    mockRoom.participants = new Map([[0, 'participant1'], [1, 'participant2']]);
+    mockRoom.participants = new Map([
+      [0, 'participant1'],
+      [1, 'participant2'],
+    ]);
     mockedVideoContext.mockImplementation(() => ({
       room: mockRoom,
     }));
@@ -37,6 +43,25 @@ describe('the useParticipants hook', () => {
       mockRoom.emit('participantDisconnected', 'participant1');
     });
     expect(result.current).toEqual(['participant2']);
+  });
+
+  it('should reorder participants when the dominant speaker changes', () => {
+    mockRoom.participants = new Map([
+      [0, 'participant1'],
+      [1, 'participant2'],
+      [2, 'participant3'],
+    ]);
+    const { result, rerender } = renderHook(useParticipants);
+    expect(result.current).toEqual(['participant1', 'participant2', 'participant3']);
+    mockUseDominantSpeaker.mockImplementation(() => 'participant2');
+    rerender();
+    expect(result.current).toEqual(['participant2', 'participant1', 'participant3']);
+    mockUseDominantSpeaker.mockImplementation(() => 'participant3');
+    rerender();
+    expect(result.current).toEqual(['participant3', 'participant2', 'participant1']);
+    mockUseDominantSpeaker.mockImplementation(() => null);
+    rerender();
+    expect(result.current).toEqual(['participant3', 'participant2', 'participant1']);
   });
 
   it('should clean up listeners on unmount', () => {
--- src/hooks/useParticipants/useParticipants.tsx
+++ src/hooks/useParticipants/useParticipants.tsx
@@ -1,10 +1,24 @@
 import { useEffect, useState } from 'react';
-import useVideoContext from '../useVideoContext/useVideoContext';
 import { RemoteParticipant } from 'twilio-video';
+import useDominantSpeaker from '../useDominantSpeaker/useDominantSpeaker';
+import useVideoContext from '../useVideoContext/useVideoContext';
 
 export default function useParticipants() {
   const { room } = useVideoContext();
+  const dominantSpeaker = useDominantSpeaker();
   const [participants, setParticipants] = useState(Array.from(room.participants.values()));
+
+  // When the dominant speaker changes, they are moved to the front of the participants array.
+  // This means that the most recent dominant speakers will always be near the top of the
+  // ParticipantStrip component.
+  useEffect(() => {
+    if (dominantSpeaker) {
+      setParticipants(prevParticipants => [
+        dominantSpeaker,
+        ...prevParticipants.filter(participant => participant !== dominantSpeaker),
+      ]);
+    }
+  }, [dominantSpeaker]);
 
   useEffect(() => {
     const participantConnected = (participant: RemoteParticipant) =>
