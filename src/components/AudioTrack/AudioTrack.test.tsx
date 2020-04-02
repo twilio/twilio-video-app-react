02a648baf7506868ae3c3896a8ca0648c2dd8943
timmydoza <tmendoza@twilio.com>
2020-04-02 15:33:21 -0600

    Update how audio track renders (#124)
    * Update how audio track renders
    
    * Disable audio tests for now

--- src/components/AudioTrack/AudioTrack.test.tsx
+++ src/components/AudioTrack/AudioTrack.test.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import AudioTrack from './AudioTrack';
 
+const audioEl = document.createElement('audio');
+const mockTrack = { attach: jest.fn(() => audioEl), detach: jest.fn(() => [audioEl]) } as any;
+
 describe('the AudioTrack component', () => {
-  it('should call the attach method when the component mounts', () => {
-    const mockTrack = { attach: jest.fn(), detach: jest.fn() } as any;
+  beforeEach(jest.clearAllMocks);
+
+  it('should add an audio element to the DOM when the component mounts', () => {
     render(<AudioTrack track={mockTrack} />);
-    expect(mockTrack.attach).toHaveBeenCalledWith(expect.any(window.HTMLAudioElement));
+    expect(mockTrack.attach).toHaveBeenCalled();
     expect(mockTrack.detach).not.toHaveBeenCalled();
+    expect(document.querySelector('audio')).toBe(audioEl);
   });
 
-  it('it should call the detach method when the component unmounts', () => {
-    const mockTrack = { attach: jest.fn(), detach: jest.fn() } as any;
+  it('should remove the audio element from the DOM when the component unmounts', () => {
     const { unmount } = render(<AudioTrack track={mockTrack} />);
     unmount();
-    expect(mockTrack.detach).toHaveBeenCalledWith(expect.any(window.HTMLAudioElement));
+    expect(mockTrack.detach).toHaveBeenCalled();
+    expect(document.querySelector('audio')).toBe(null);
   });
 });
