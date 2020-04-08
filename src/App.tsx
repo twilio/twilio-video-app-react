db6ffdce0ca7ebdc962064c6d0ea5b9e6251e0f6
Tim Mendoza <tmendoza@twilio.com>
2020-04-07 18:03:39 -0600

    Add comment

--- src/App.tsx
+++ src/App.tsx
@@ -37,6 +37,12 @@ export function useHeight() {
 
 export default function App() {
   const roomState = useRoomState();
+
+  // Here we would like the height of the main container to be the height of the viewport.
+  // On some mobile browsers, 'height: 100vh' sets the height equal to that of the screen,
+  // not the viewport. This looks bad when the mobile browsers location bar is open.
+  // We will dynamically set the height with 'window.innerHeight', which means that this
+  // will look good on mobile browsers even after the location bar opens or closes.
   const height = useHeight();
 
   return (
