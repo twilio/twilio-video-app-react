72bbb07a8aa85e29affab15c5a3d7ed42a969ae5
Tim Mendoza <tmendoza@twilio.com>
2020-04-09 11:05:10 -0600

    Add new build script to remove part of build message

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ import { VideoProvider } from './components/VideoProvider';
 // See: https://media.twiliocdn.com/sdk/js/video/releases/2.0.0/docs/global.html#ConnectOptions
 // for available connection options.
 const connectionOptions: ConnectOptions = {
-  bandwidthProfile: {
+  bandwidthPrfofile: {
     video: {
       mode: 'collaboration',
       dominantSpeakerPriority: 'standard',
