7906194018c7d9b4d58f5187d37c57ce5748f30a
Tim Mendoza <tmendoza@twilio.com>
2020-04-17 16:41:29 -0600

    Add comments

--- src/components/VideoProvider/AttachVisibilityHandler/AttachVisibilityHandler.tsx
+++ src/components/VideoProvider/AttachVisibilityHandler/AttachVisibilityHandler.tsx
@@ -3,6 +3,16 @@ import { useEffect, useRef } from 'react';
 import useLocalVideoToggle from '../../../hooks/useLocalVideoToggle/useLocalVideoToggle';
 import useVideoContext from '../../../hooks/useVideoContext/useVideoContext';
 
+/* 
+  This component adds a visibilitychange handler to the document when
+  the user is using a mobile device. When the handler detects that
+  the browser has been backgrounded, it unpublishes the users local
+  video track. The browser cannot send video to the room when it has
+  been backgrounded, so unpublishing the track stops video capture
+  on the device, and triggers a UI update for all other participants
+  to show that this user's video track has been turned off.
+*/
+
 export default function AttachVisibilityHandler() {
   const { room } = useVideoContext();
   const [isVideoEnabled, toggleVideoEnabled] = useLocalVideoToggle();
@@ -11,9 +21,12 @@ export default function AttachVisibilityHandler() {
   useEffect(() => {
     if (isMobile) {
       const handleVisibilityChange = () => {
+        // We don't need to unpublish the local video track if it has already been unpublished
         if (document.visibilityState === 'hidden' && isVideoEnabled) {
           shouldRepublishVideoOnForeground.current = true;
           toggleVideoEnabled();
+
+          // Don't publish the local video track if it wasn't published before the app was backgrounded
         } else if (shouldRepublishVideoOnForeground.current) {
           shouldRepublishVideoOnForeground.current = false;
           toggleVideoEnabled();
