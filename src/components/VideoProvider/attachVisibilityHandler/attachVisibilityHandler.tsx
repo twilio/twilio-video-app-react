0f44f1f725ad4e6592507bcc7503db25d1512f80
Tim Mendoza <tmendoza@twilio.com>
2020-04-17 14:43:40 -0600

    Correctly use document.addEventListene for visibilitychanges

--- src/components/VideoProvider/attachVisibilityHandler/attachVisibilityHandler.tsx
+++ src/components/VideoProvider/attachVisibilityHandler/attachVisibilityHandler.tsx
@@ -20,9 +20,9 @@ export default function AttachVisibilityHandler() {
         }
       };
 
-      window.addEventListener('visibilitychange', handleVisibilityChange);
+      document.addEventListener('visibilitychange', handleVisibilityChange);
       return () => {
-        window.removeEventListener('visibilitychange', handleVisibilityChange);
+        document.removeEventListener('visibilitychange', handleVisibilityChange);
       };
     }
   }, [isVideoEnabled, room, toggleVideoEnabled]);
