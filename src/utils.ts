9e5b17624e337bdba7c0ffa79864bc19101e206a
Tim Mendoza <tmendoza@twilio.com>
2020-04-17 14:01:49 -0600

    Remove unused browserName function from utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,3 @@
-export const browserName = (() => {
-  if (typeof navigator === 'undefined' || typeof navigator.userAgent !== 'string') {
-    return null;
-  }
-  if (/Chrome|CriOS/.test(navigator.userAgent)) {
-    return 'chrome';
-  }
-  if (/Firefox|FxiOS/.test(navigator.userAgent)) {
-    return 'firefox';
-  }
-  if (/Safari/.test(navigator.userAgent)) {
-    return 'safari';
-  }
-  return null;
-})();
-
 export const isMobile = (() => {
   if (typeof navigator === 'undefined' || typeof navigator.userAgent !== 'string') {
     return false;
