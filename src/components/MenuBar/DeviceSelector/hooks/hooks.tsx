7e69b664ac512148a36a21dfe4e9e857a139741b
Tim Mendoza <tmendoza@twilio.com>
2020-04-28 16:33:10 -0600

    Move ensureMediaPermissions to utils file

--- src/components/MenuBar/DeviceSelector/hooks/hooks.tsx
+++ src/components/MenuBar/DeviceSelector/hooks/hooks.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
-import useVideoContext from '../../../../hooks/useVideoContext/useVideoContext';
+import { ensureMediaPermissions } from '../../../../utils';
 
 export function useDevices() {
   const [devices, setDevices] = useState<MediaDeviceInfo[]>([]);
 
   useEffect(() => {
-    navigator.mediaDevices.enumerateDevices().then(devices => setDevices(devices));
+    ensureMediaPermissions().then(() => navigator.mediaDevices.enumerateDevices().then(devices => setDevices(devices)));
   }, []);
 
   return devices;
@@ -24,36 +24,4 @@ export function useVideoInputDevices() {
 export function useAudioOutputDevices() {
   const devices = useDevices();
   return devices.filter(device => device.kind === 'audiooutput');
-}
-
-export function useMediaStreamTrack(deviceInfo: MediaDeviceInfo) {
-  const { localTracks } = useVideoContext();
-  const [mediaStreamTrack, setMediaStreamTrack] = useState<MediaStreamTrack | null>(null);
-
-  const existingTrack = localTracks.find(
-    track => track.mediaStreamTrack.getSettings().deviceId === deviceInfo.deviceId
-  );
-
-  useEffect(() => {
-    let newTrack: MediaStreamTrack;
-
-    if (existingTrack) {
-      setMediaStreamTrack(existingTrack.mediaStreamTrack);
-    } else {
-      const type: 'audio' | 'video' = deviceInfo.kind.includes('audio') ? 'audio' : 'video';
-
-      navigator.mediaDevices.getUserMedia({ [type]: { deviceId: { exact: deviceInfo.deviceId } } }).then(device => {
-        newTrack = device.getTracks()[0];
-        setMediaStreamTrack(newTrack);
-      });
-    }
-
-    return () => {
-      if (newTrack) {
-        newTrack.stop();
-      }
-    };
-  }, [deviceInfo, existingTrack]);
-
-  return mediaStreamTrack;
 }
--- src/components/VideoProvider/useLocalTracks/useLocalTracks.ts
+++ src/components/VideoProvider/useLocalTracks/useLocalTracks.ts
@@ -1,21 +1,7 @@
 import { useCallback, useEffect, useState } from 'react';
-import Video, { LocalVideoTrack, LocalAudioTrack, CreateLocalTrackOptions } from 'twilio-video';
 
-// This function ensures that the user has granted the browser permission to use audio and video
-// devices. If permission has not been granted, it will cause the browser to ask for permission
-// for audio and video at the same time (as opposed to separate requests).
-function ensureMediaPermissions() {
-  return navigator.mediaDevices
-    .enumerateDevices()
-    .then(devices => devices.every(device => !(device.deviceId && device.label)))
-    .then(shouldAskForMediaPermissions => {
-      if (shouldAskForMediaPermissions) {
-        navigator.mediaDevices
-          .getUserMedia({ audio: true, video: true })
-          .then(mediaStream => mediaStream.getTracks().forEach(track => track.stop()));
-      }
-    });
-}
+import { ensureMediaPermissions } from '../../../utils';
+import Video, { LocalVideoTrack, LocalAudioTrack, CreateLocalTrackOptions } from 'twilio-video';
 
 export function useLocalAudioTrack() {
   const [track, setTrack] = useState<LocalAudioTrack>();
