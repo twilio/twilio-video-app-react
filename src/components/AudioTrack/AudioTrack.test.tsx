6b7da517882001c058f3e57260739ba375c41bf1
Tim Mendoza <tmendoza@twilio.com>
2020-04-28 22:01:17 -0600

    Add tests for setSinkId

--- src/components/AudioTrack/AudioTrack.test.tsx
+++ src/components/AudioTrack/AudioTrack.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import AudioTrack from './AudioTrack';
+import { useAppState } from '../../state';
 
 const audioEl = document.createElement('audio');
+audioEl.setSinkId = jest.fn();
+
 const mockTrack = { attach: jest.fn(() => audioEl), detach: jest.fn(() => [audioEl]) } as any;
+
+jest.mock('../../state');
+const mockUseAppState = useAppState as jest.Mock<any>;
+
+mockUseAppState.mockImplementation(() => ({ activeSinkId: '' }));
 
 describe('the AudioTrack component', () => {
   beforeEach(jest.clearAllMocks);
@@ -13,6 +21,7 @@ describe('the AudioTrack component', () => {
     expect(mockTrack.attach).toHaveBeenCalled();
     expect(mockTrack.detach).not.toHaveBeenCalled();
     expect(document.querySelector('audio')).toBe(audioEl);
+    expect(audioEl.setSinkId).not.toHaveBeenCalledWith('mock-sink-id');
   });
 
   it('should remove the audio element from the DOM when the component unmounts', () => {
@@ -20,5 +29,13 @@ describe('the AudioTrack component', () => {
     unmount();
     expect(mockTrack.detach).toHaveBeenCalled();
     expect(document.querySelector('audio')).toBe(null);
+  });
+
+  describe('with an activeSinkId', () => {
+    it('should set the sinkId when the component mounts', () => {
+      mockUseAppState.mockImplementationOnce(() => ({ activeSinkId: 'mock-sink-id' }));
+      render(<AudioTrack track={mockTrack} />);
+      expect(audioEl.setSinkId).toHaveBeenCalledWith('mock-sink-id');
+    });
   });
 });
