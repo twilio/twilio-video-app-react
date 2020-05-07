05534943c30c8dd85f5c1b0de9247db1ca5a768d
Tim Mendoza <tmendoza@twilio.com>
2020-05-06 18:11:06 -0600

    Add track name to audio elements

--- src/components/AudioTrack/AudioTrack.tsx
+++ src/components/AudioTrack/AudioTrack.tsx
@@ -12,6 +12,7 @@ export default function AudioTrack({ track }: AudioTrackProps) {
 
   useEffect(() => {
     audioEl.current = track.attach();
+    audioEl.current.setAttribute('data-cy-audio-track-name', track.name);
     document.body.appendChild(audioEl.current);
     return () => track.detach().forEach(el => el.remove());
   }, [track]);
