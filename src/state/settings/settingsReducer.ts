b1183e7119c974403d6144a2ee564c465e6770a6
Tim Mendoza <tmendoza@twilio.com>
2020-05-12 12:20:18 -0600

    Add settings reducer and add to AppState

---
+++ src/state/settings/settingsReducer.ts
@@ -0,0 +1,43 @@
+import { Track, VideoBandwidthProfileOptions } from 'twilio-video';
+
+export interface Settings {
+  trackSwitchOffMode: VideoBandwidthProfileOptions['trackSwitchOffMode'];
+  dominantSpeakerPriority: Track.Priority;
+  bandwidthProfileMode: VideoBandwidthProfileOptions['mode'];
+  maxTracks: string;
+  maxAudioBitrate: string;
+  renderDimensionLow: Track.Priority;
+  renderDimensionStandard: Track.Priority;
+  renderDimensionHigh: Track.Priority;
+}
+
+export interface SettingsAction {
+  name: keyof Settings;
+  value: string;
+}
+
+export const initialSettings: Settings = {
+  trackSwitchOffMode: undefined,
+  dominantSpeakerPriority: 'standard',
+  bandwidthProfileMode: 'collaboration',
+  maxTracks: '10',
+  maxAudioBitrate: '16000',
+  renderDimensionLow: 'low',
+  renderDimensionStandard: 'standard',
+  renderDimensionHigh: 'high',
+};
+
+export const labels = (() => {
+  const target: any = {};
+  for (const setting in initialSettings) {
+    target[setting] = setting as keyof Settings;
+  }
+  return target as { [key in keyof Settings]: string };
+})();
+
+export function settingsReducer(state: Settings, action: SettingsAction) {
+  return {
+    ...state,
+    [action.name]: action.value === 'default' ? undefined : action.value,
+  };
+}
