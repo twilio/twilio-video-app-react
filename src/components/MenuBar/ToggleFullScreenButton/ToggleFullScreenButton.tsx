2919175e96da036c9fbce9564ca8c5bb8384fdc2
timmydoza <tmendoza@twilio.com>
2020-05-18 17:00:04 -0600

    Fix ios fullscreen (#212)
    * Render FullScreenButton only when API is supported
    
    * Add test
    
    * Cleanup
    
    * Add comment

--- src/components/MenuBar/ToggleFullScreenButton/ToggleFullScreenButton.tsx
+++ src/components/MenuBar/ToggleFullScreenButton/ToggleFullScreenButton.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import FullscreenIcon from '@material-ui/icons/Fullscreen';
+import fscreen from 'fscreen';
 import FullscreenExitIcon from '@material-ui/icons/FullscreenExit';
 import IconButton from '@material-ui/core/IconButton';
 
@@ -9,9 +10,9 @@ import useFullScreenToggle from '../../../hooks/useFullScreenToggle/useFullScree
 export default function ToggleFullscreenButton() {
   const [isFullScreen, toggleFullScreen] = useFullScreenToggle();
 
-  return (
+  return fscreen.fullscreenEnabled ? (
     <IconButton aria-label={`full screen`} onClick={toggleFullScreen}>
       {isFullScreen ? <FullscreenExitIcon /> : <FullscreenIcon />}
     </IconButton>
-  );
+  ) : null;
 }
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,6 @@ import Adapter from 'enzyme-adapter-react-16';
 import 'isomorphic-fetch';
 
 configure({ adapter: new Adapter() });
+
+// Mocks the Fullscreen API. This is needed for ToggleFullScreenButton.test.tsx.
+Object.defineProperty(document, 'fullscreenEnabled', { value: true, writable: true });
