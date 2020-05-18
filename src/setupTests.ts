aa754f8ff0b615b4e8b54190d2b5fbdf88916db7
Tim Mendoza <tmendoza@twilio.com>
2020-05-18 15:40:53 -0600

    Add comment

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,4 +4,5 @@ import 'isomorphic-fetch';
 
 configure({ adapter: new Adapter() });
 
+// Mocks the Fullscreen API. This is needed for ToggleFullScreenButton.test.tsx.
 Object.defineProperty(document, 'fullscreenEnabled', { value: true, writable: true });
