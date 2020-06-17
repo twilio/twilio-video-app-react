fe37ff279c64b5c4a84ea4abf01741a72ee7cefe
Tim Mendoza <tmendoza@twilio.com>
2020-06-17 16:23:11 -0600

    Add useIsLocalTrackStopped to useLocalAudioToggle

--- src/hooks/useLocalAudioToggle/useLocalAudioToggle.tsx
+++ src/hooks/useLocalAudioToggle/useLocalAudioToggle.tsx
@@ -2,17 +2,19 @@ import { LocalAudioTrack } from 'twilio-video';
 import { useCallback } from 'react';
 import useIsTrackEnabled from '../useIsTrackEnabled/useIsTrackEnabled';
 import useVideoContext from '../useVideoContext/useVideoContext';
+import useIsLocalTrackStopped from '../useIsLocalTrackStopped/useIsLocalTrackStopped';
 
 export default function useLocalAudioToggle() {
   const { localTracks } = useVideoContext();
   const audioTrack = localTracks.find(track => track.kind === 'audio') as LocalAudioTrack;
   const isEnabled = useIsTrackEnabled(audioTrack);
+  const isStopped = useIsLocalTrackStopped(audioTrack);
 
   const toggleAudioEnabled = useCallback(() => {
-    if (audioTrack) {
+    if (audioTrack && !isStopped) {
       audioTrack.isEnabled ? audioTrack.disable() : audioTrack.enable();
     }
-  }, [audioTrack]);
+  }, [audioTrack, isStopped]);
 
-  return [isEnabled, toggleAudioEnabled] as const;
+  return [isEnabled && !isStopped, toggleAudioEnabled] as const;
 }
