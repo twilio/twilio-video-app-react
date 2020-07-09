28304c940f608e6db32b0c2416d29eed1b4c2124
Tim Mendoza <tmendoza@twilio.com>
2020-07-09 14:13:34 -0600

    Add comments to useMediaStreamTrack hook

--- src/hooks/useMediaStreamTrack/useMediaStreamTrack.ts
+++ src/hooks/useMediaStreamTrack/useMediaStreamTrack.ts
@@ -1,6 +1,13 @@
 import { useState, useEffect } from 'react';
 import { AudioTrack, VideoTrack } from 'twilio-video';
 
+/*
+ * This hook allows components to reliably use the 'mediaStreamTrack' property of
+ * an AudioTrack or a VideoTrack. Whenever 'localTrack.restart(...)' is called, it
+ * will replace the mediaStreamTrack property of the localTrack, but the localTrack
+ * object will stay the same. Therefore this hook is needed in order for components
+ * to rerender in response to the mediaStreamTrack changing.
+ */
 export default function useMediaStreamTrack(track?: AudioTrack | VideoTrack) {
   const [mediaStreamTrack, setMediaStreamTrack] = useState(track?.mediaStreamTrack);
 
@@ -9,7 +16,6 @@ export default function useMediaStreamTrack(track?: AudioTrack | VideoTrack) {
 
     if (track) {
       const handleStarted = () => setMediaStreamTrack(track.mediaStreamTrack);
-
       track.on('started', handleStarted);
       return () => {
         track.off('started', handleStarted);
