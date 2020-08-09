9f755b7c717bb701e37f124f75da9066f77fc5fe
gila1997 <gdadon@escribers.net>
2020-08-09 12:34:49 +0300

    fix bug

--- src/state/index.tsx
+++ src/state/index.tsx
@@ -1,128 +1,121 @@
 import React, { createContext, useContext, useReducer, useState } from 'react';
 import { TwilioError } from 'twilio-video';
 import axios from 'axios';
-import jwt from 'jsonwebtoken';
+import * as jwt_decode from 'jwt-decode';
 
 export interface StateContextType {
-    error: TwilioError | null;
-    setError(error: TwilioError | null): void;
-    isFetching: boolean;
-    setSelectedAudioInput: string;
-    selectedVideoInput: string;
-    setSelectedVideoInput: string;
-    selectedSpeakerOutput: string;
-    setSelectedSpeakerOutput: string;
-    gridView: boolean;
-    setGridView: any;
-    getToken(caseNumber, partyType, partyName, pinNumber): Promise<string>;
-    removeParticipant: any;
+  error: TwilioError | null;
+  setError(error: TwilioError | null): void;
+  isFetching: boolean;
+  setSelectedAudioInput: string;
+  selectedVideoInput: string;
+  setSelectedVideoInput: string;
+  selectedSpeakerOutput: string;
+  setSelectedSpeakerOutput: string;
+  gridView: boolean;
+  setGridView: any;
+  getToken(caseNumber, partyType, partyName, pinNumber): Promise<string>;
+  removeParticipant: any;
 }
 
-export const StateContext = createContext<StateContextType>(null!);;
+export const StateContext = createContext<StateContextType>(null!);
 
 export default function AppStateProvider(props: React.PropsWithChildren<{}>) {
-    const [error, setError] = useState<TwilioError | null>(null);
-    const [isFetching, setIsFetching] = useState(false);
-    // eslint-disable-next-line no-unused-vars
-    const [user, setUser] = useState(null);
-    const [gridView, setGridView] = useState(true);
-    const [userToken, setUserToken] = useState('');
-    const [selectedAudioInput, setSelectedAudioInput] = useState({ deviceId: '' });
-    const [selectedVideoInput, setSelectedVideoInput] = useState({ deviceId: '' });
-    const [selectedSpeakerOutput, setSelectedSpeakerOutput] = useState({ deviceId: '' });
+  const [error, setError] = useState<TwilioError | null>(null);
+  const [isFetching, setIsFetching] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [user, setUser] = useState(null);
+  const [gridView, setGridView] = useState(true);
+  const [userToken, setUserToken] = useState('');
+  const [selectedAudioInput, setSelectedAudioInput] = useState({ deviceId: '' });
+  const [selectedVideoInput, setSelectedVideoInput] = useState({ deviceId: '' });
+  const [selectedSpeakerOutput, setSelectedSpeakerOutput] = useState({ deviceId: '' });
 
-    const reporterToken = window.location.hash.substr(1);
+  const reporterToken = window.location.hash.substr(1);
 
-    let contextValue = {
-        error,
-        setError,
-        isFetching,
-        selectedAudioInput,
-        setSelectedAudioInput,
-        selectedVideoInput,
-        setSelectedVideoInput,
-        selectedSpeakerOutput,
-        setSelectedSpeakerOutput,
-        gridView,
-        setGridView,
-        getToken: async (caseNumber, partyType, partyName, pinNumber = '') => {
-            const endpoint = `${process.env.REACT_APP_API_URL}/token`;
+  let contextValue = ({
+    error,
+    setError,
+    isFetching,
+    selectedAudioInput,
+    setSelectedAudioInput,
+    selectedVideoInput,
+    setSelectedVideoInput,
+    selectedSpeakerOutput,
+    setSelectedSpeakerOutput,
+    gridView,
+    setGridView,
+    getToken: async (caseNumber, partyType, partyName, pinNumber = '') => {
+      const endpoint = `${process.env.REACT_APP_API_URL}/token`;
 
-            const { data } = await axios({
-                url: endpoint,
-                method: 'POST',
-                headers: {
-                    Authorization: reporterToken ? `Bearer ${reporterToken}` : '',
-                },
-                data: {
-                    caseNumber,
-                    partyType,
-                    partyName,
-                    pinNumber
-                },
-            });
-
-            return data;
+      const { data } = await axios({
+        url: endpoint,
+        method: 'POST',
+        headers: {
+          Authorization: reporterToken ? `Bearer ${reporterToken}` : '',
         },
-        removeParticipant: async (participantSid) => {
-            const endpoint = `${process.env.REACT_APP_API_URL}/remove-participant`;
-
-            const { data } = await axios({
-                url: endpoint,
-                method: 'POST',
-                headers: {
-                    Authorization: `Bearer ${userToken}`,
-                },
-                data: { participantSid },
-            });
-
-            return data;
+        data: {
+          caseNumber,
+          partyType,
+          partyName,
+          pinNumber,
         },
-    } as unknown as StateContextType;
+      });
 
-    const getToken: StateContextType['getToken'] = (caseNumber, partyType, partyName, pinNumber = '') => {
-        setIsFetching(true);
-        return contextValue
-            .getToken(
-                caseNumber,
-                partyType === 'Reporter' ? 'Other' : partyType,
-                partyName,
-                pinNumber
-            )
-            .then((res: any) => {
-                setIsFetching(false);
-                setUserToken(res.token);
-                const user = jwt.decode(res.token);
-                setUser(user);
-                return Promise.resolve(user.twilioToken);
-            })
-            .catch((err) => {
-                setError(err.response ? err.response.data : { message: 'Network Error' });
-                setIsFetching(false);
-                return Promise.reject(err);
-            });
-    };
+      return data;
+    },
+    removeParticipant: async participantSid => {
+      const endpoint = `${process.env.REACT_APP_API_URL}/remove-participant`;
 
-    const removeParticipant: StateContextType['removeParticipant'] = (participantSid) => {
-        return contextValue.removeParticipant(participantSid).catch((err) => {
-            setError(err.response ? err.response.data : { message: 'Network Error' });
-            return Promise.reject(err);
-        });
-    };
+      const { data } = await axios({
+        url: endpoint,
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+        data: { participantSid },
+      });
 
-    return (
-        <StateContext.Provider
-            value={{ ...contextValue, getToken, removeParticipant }}
-        >
-            {props.children}
-        </StateContext.Provider>
-    );
+      return data;
+    },
+  } as unknown) as StateContextType;
+
+  const getToken: StateContextType['getToken'] = (caseNumber, partyType, partyName, pinNumber = '') => {
+    setIsFetching(true);
+    return contextValue
+      .getToken(caseNumber, partyType === 'Reporter' ? 'Other' : partyType, partyName, pinNumber)
+      .then((res: any) => {
+        setIsFetching(false);
+        setUserToken(res.token);
+        const user = jwt_decode(res.token);
+        setUser(user);
+        return Promise.resolve(user.twilioToken);
+      })
+      .catch(err => {
+        setError(err.response ? err.response.data : { message: 'Network Error' });
+        setIsFetching(false);
+        return Promise.reject(err);
+      });
+  };
+
+  const removeParticipant: StateContextType['removeParticipant'] = participantSid => {
+    return contextValue.removeParticipant(participantSid).catch(err => {
+      setError(err.response ? err.response.data : { message: 'Network Error' });
+      return Promise.reject(err);
+    });
+  };
+
+  return (
+    <StateContext.Provider value={{ ...contextValue, getToken, removeParticipant }}>
+      {props.children}
+    </StateContext.Provider>
+  );
 }
 
-export function useAppState():any {
-    const context = useContext(StateContext);
-    if (!context) {
-        throw new Error('useAppState must be used within the AppStateProvider');
-    }
-    return context;
+export function useAppState(): any {
+  const context = useContext(StateContext);
+  if (!context) {
+    throw new Error('useAppState must be used within the AppStateProvider');
+  }
+  return context;
 }
