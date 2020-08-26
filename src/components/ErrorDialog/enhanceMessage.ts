fc9c03b38dbbe2ab56aa300c24524a6ffbf8af20
gila1997 <gdadon@escribers.net>
2020-08-26 12:30:34 +0300

    CHANGE ERROR

--- src/components/ErrorDialog/enhanceMessage.ts
+++ src/components/ErrorDialog/enhanceMessage.ts
@@ -1,10 +1,10 @@
-import { EROOR_MESSAGE } from '../../utils/displayStrings';
+import { ERROR_MESSAGE } from '../../utils/displayStrings';
 // This function is used to provide error messages to the user that are
 // different than the error messages provided by the SDK.
 export default function enhanceMessage(message = '', code?: number) {
   switch (code) {
     case 20101: // Invalid token error
-      return message + EROOR_MESSAGE.INVALID_CREDENTIALS;
+      return message + ERROR_MESSAGE.INVALID_CREDENTIALS;
     default:
       return message;
   }
