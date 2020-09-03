a85c34a880616e7d48f02ea7d4c03ea0c3423ec1
Tim Mendoza <tmendoza@twilio.com>
2020-09-02 21:10:54 -0600

    Add IntroContainer component to login page

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -4,12 +4,10 @@ import { useAppState } from '../../state';
 import Button from '@material-ui/core/Button';
 import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import { ReactComponent as GoogleLogo } from './google-logo.svg';
-import { ReactComponent as TwilioLogo } from './twilio-logo.svg';
+import IntroContainer from '../IntroContainer/IntroContainer';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-import videoLogo from './video-logo.png';
 
 import { makeStyles } from '@material-ui/core/styles';
 import { useLocation, useHistory } from 'react-router-dom';
@@ -37,11 +35,19 @@ const useStyles = makeStyles({
     background: 'white',
     color: 'black',
   },
-  button: {
-    color: 'black',
+  googleButton: {
     background: 'white',
-    margin: '0.8em 0 0.7em',
+    color: 'rgb(0, 94, 166)',
+    borderRadius: '4px',
+    border: '2px solid rgb(2, 122, 197)',
+    margin: '1.8em 0 0.7em',
     textTransform: 'none',
+    boxShadow: 'none',
+    padding: '0.3em 1em',
+    '&:hover': {
+      background: 'white',
+      boxShadow: 'none',
+    },
   },
   errorMessage: {
     color: 'red',
@@ -51,6 +57,9 @@ const useStyles = makeStyles({
     '& svg': {
       marginRight: '0.4em',
     },
+  },
+  gutterBottom: {
+    marginBottom: '1em',
   },
 });
 
@@ -87,41 +96,51 @@ export default function LoginPage() {
   }
 
   return (
-    <Grid container justify="center" alignItems="flex-start" className={classes.container}>
-      <Paper className={classes.paper} elevation={6}>
-        <TwilioLogo className={classes.twilioLogo} />
-        <img className={classes.videoLogo} src={videoLogo} alt="Video Logo"></img>
-
-        {process.env.REACT_APP_SET_AUTH === 'firebase' && (
-          <Button variant="contained" className={classes.button} onClick={login} startIcon={<GoogleLogo />}>
+    <IntroContainer>
+      {process.env.REACT_APP_SET_AUTH === 'firebase' && (
+        <>
+          <Typography variant="h5" className={classes.gutterBottom}>
+            Sign in to join a room
+          </Typography>
+          <Typography variant="body1">Sign in using your Twilio Google Account</Typography>
+          <Button variant="contained" className={classes.googleButton} onClick={login} startIcon={<GoogleLogo />}>
             Sign in with Google
           </Button>
-        )}
+        </>
+      )}
 
-        {process.env.REACT_APP_SET_AUTH === 'passcode' && (
-          <form onSubmit={handleSubmit}>
-            <Grid container alignItems="center" direction="column">
-              <TextField
-                id="input-passcode"
-                label="Passcode"
-                onChange={(e: ChangeEvent<HTMLInputElement>) => setPasscode(e.target.value)}
-                type="password"
-              />
+      {process.env.REACT_APP_SET_AUTH === 'passcode' && (
+        <form onSubmit={handleSubmit}>
+          <>
+            <Typography variant="h5" className={classes.gutterBottom}>
+              Enter passcode to join a room
+            </Typography>
+            <Grid container justify="space-between">
               <div>
-                {authError && (
-                  <Typography variant="caption" className={classes.errorMessage}>
-                    <ErrorOutlineIcon />
-                    {authError.message}
-                  </Typography>
-                )}
+                <TextField
+                  id="input-passcode"
+                  label="Passcode"
+                  onChange={(e: ChangeEvent<HTMLInputElement>) => setPasscode(e.target.value)}
+                  type="password"
+                />
+                <div>
+                  {authError && (
+                    <Typography variant="caption" className={classes.errorMessage}>
+                      <ErrorOutlineIcon />
+                      {authError.message}
+                    </Typography>
+                  )}
+                </div>
               </div>
-              <Button variant="contained" className={classes.button} type="submit" disabled={!passcode.length}>
-                Submit
-              </Button>
+              <div>
+                <Button variant="contained" color="primary" type="submit" disabled={!passcode.length}>
+                  Submit
+                </Button>
+              </div>
             </Grid>
-          </form>
-        )}
-      </Paper>
-    </Grid>
+          </>
+        </form>
+      )}
+    </IntroContainer>
   );
 }
