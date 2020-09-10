1a55f7d19d35770368281a44d3aaa13d1602e3cd
Tim Mendoza <tmendoza@twilio.com>
2020-09-09 21:35:30 -0600

    Cleanup Menu Component

--- src/components/MenuBar/Menu/Menu.tsx
+++ src/components/MenuBar/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from 'react';
+import React, { useState, useRef } from 'react';
 import AboutDialog from '../AboutDialog/AboutDialog';
 import Button from '@material-ui/core/Button';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
@@ -7,24 +7,12 @@ import MenuItem from '@material-ui/core/MenuItem';
 import SettingsDialog from '../SettingsDialog/SettingsDialog';
 import Typography from '@material-ui/core/Typography';
 
-import { useAppState } from '../../../state';
-import useVideoContext from '../../../hooks/useVideoContext/useVideoContext';
-
 export default function Menu() {
-  const { signOut } = useAppState();
-  const { room, localTracks } = useVideoContext();
-
   const [aboutOpen, setAboutOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
   const [settingsOpen, setSettingsOpen] = useState(false);
 
   const anchorRef = useRef<HTMLButtonElement>(null);
-
-  const handleSignOut = useCallback(() => {
-    room.disconnect?.();
-    localTracks.forEach(track => track.stop());
-    signOut?.();
-  }, [room.disconnect, localTracks, signOut]);
 
   return (
     <>
