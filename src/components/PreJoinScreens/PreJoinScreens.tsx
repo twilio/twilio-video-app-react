784a7ca2c14e295085eb8fed68c0f33e9bf66c6f
Tim Mendoza <tmendoza@twilio.com>
2020-09-09 21:31:18 -0600

    Add RoomName and DeviceSelection screens

---
+++ src/components/PreJoinScreens/PreJoinScreens.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect, FormEvent } from 'react';
+import DeviceSelectionScreen from './DeviceSelectionScreen/DeviceSelectionScreen';
+import IntroContainer from '../IntroContainer/IntroContainer';
+import RoomNameScreen from './RoomNameScreen/RoomNameScreen';
+import { useAppState } from '../../state';
+import { useParams } from 'react-router-dom';
+
+export enum Steps {
+  roomNameStep,
+  deviceSelectionStep,
+}
+
+export default function PreJoinScreens() {
+  const { user } = useAppState();
+  const { URLRoomName } = useParams();
+  const [step, setStep] = useState(Steps.roomNameStep);
+
+  const [name, setName] = useState<string>(user?.displayName || '');
+  const [roomName, setRoomName] = useState<string>('');
+
+  useEffect(() => {
+    if (URLRoomName) {
+      setRoomName(URLRoomName);
+    }
+  }, [URLRoomName]);
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // If this app is deployed as a twilio function, don't change the URL because routing isn't supported.
+    if (!window.location.origin.includes('twil.io')) {
+      window.history.replaceState(null, '', window.encodeURI(`/room/${roomName}${window.location.search || ''}`));
+    }
+    setStep(Steps.deviceSelectionStep);
+  };
+
+  return (
+    <IntroContainer>
+      {step === Steps.roomNameStep && (
+        <RoomNameScreen
+          name={name}
+          roomName={roomName}
+          setName={setName}
+          setRoomName={setRoomName}
+          handleSubmit={handleSubmit}
+        />
+      )}
+
+      {step === Steps.deviceSelectionStep && (
+        <DeviceSelectionScreen name={name} roomName={roomName} setStep={setStep} />
+      )}
+    </IntroContainer>
+  );
+}
