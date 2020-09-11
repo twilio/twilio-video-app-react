0e81d66c4ba868d78146354bef20ca36d727b1ea
Tim Mendoza <tmendoza@twilio.com>
2020-09-10 20:31:48 -0600

    Create useGetPreflightTokens Install nanoid Install SDK branch with preflight

---
+++ src/components/PreJoinScreens/PreflightTest/useGetPreflightTokens/useGetPreflightTokens.ts
@@ -0,0 +1,27 @@
+import { nanoid } from 'nanoid';
+import { useAppState } from '../../../../state';
+import { useState, useEffect } from 'react';
+
+export default function useGetPreflightTokens() {
+  const { getToken } = useAppState();
+  const [tokens, setTokens] = useState<[string, string]>();
+  const [tokenError, setTokenError] = useState<Error>();
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+    if (!isFetching && !tokens) {
+      const roomName = 'preflight-network-test-' + nanoid();
+
+      setIsFetching(true);
+
+      Promise.all([getToken(nanoid(), roomName), getToken(nanoid(), roomName)])
+        .then(tokens => {
+          setTokens(tokens);
+          setIsFetching(false);
+        })
+        .catch(error => setTokenError(error));
+    }
+  }, [getToken, isFetching, tokens]);
+
+  return { tokens, tokenError };
+}
