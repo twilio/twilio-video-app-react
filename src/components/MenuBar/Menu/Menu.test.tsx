e457db617488844018da6e5c07cdbe56246f6da3
Tim Mendoza <tmendoza@twilio.com>
2020-09-21 15:57:13 -0600

    Update menu tests

--- src/components/MenuBar/Menu/Menu.test.tsx
+++ src/components/MenuBar/Menu/Menu.test.tsx
@@ -1,50 +1,36 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import AboutDialog from '../AboutDialog/AboutDialog';
+import { Button, MenuItem } from '@material-ui/core';
 import Menu from './Menu';
-import { useAppState } from '../../../state';
-import useVideoContext from '../../../hooks/useVideoContext/useVideoContext';
-import { MenuItem } from '@material-ui/core';
-
-jest.mock('../../../state');
-jest.mock('../../../hooks/useVideoContext/useVideoContext');
-
-const mockUseAppState = useAppState as jest.Mock<any>;
-const mockUseVideoContext = useVideoContext as jest.Mock<any>;
+import MenuContainer from '@material-ui/core/Menu';
+import SettingsDialog from '../SettingsDialog/SettingsDialog';
+import { shallow } from 'enzyme';
 
 describe('the Menu component', () => {
-  const mockDisconnect = jest.fn();
-  const mockTrack = { stop: jest.fn() };
-  mockUseVideoContext.mockImplementation(() => ({ room: { disconnect: mockDisconnect }, localTracks: [mockTrack] }));
-
-  describe('when there is a user', () => {
-    it('should include the logout button in the menu', () => {
-      mockUseAppState.mockImplementation(() => ({ user: { displayName: 'Test User' }, signOut: jest.fn() }));
-      const wrapper = shallow(<Menu />);
-      expect(wrapper.contains('Logout')).toBe(true);
-    });
-
-    it('should disconnect from the room and stop all tracks when the Logout button is clicked', () => {
-      const mockSignOut = jest.fn(() => Promise.resolve());
-      mockUseAppState.mockImplementation(() => ({
-        user: { displayName: 'Test User' },
-        signOut: mockSignOut,
-      }));
-      const wrapper = shallow(<Menu />);
-      wrapper
-        .find(MenuItem)
-        .last()
-        .simulate('click');
-      expect(mockDisconnect).toHaveBeenCalled();
-      expect(mockTrack.stop).toHaveBeenCalled();
-      expect(mockSignOut).toHaveBeenCalled();
-    });
+  it('should open the Menu when the Button is clicked', () => {
+    const wrapper = shallow(<Menu />);
+    expect(wrapper.find(MenuContainer).prop('open')).toBe(false);
+    wrapper.find(Button).simulate('click');
+    expect(wrapper.find(MenuContainer).prop('open')).toBe(true);
   });
 
-  describe('when there is not a user', () => {
-    it('should not include the logout button in the menu', () => {
-      mockUseAppState.mockImplementation(() => ({ user: null }));
-      const wrapper = shallow(<Menu />);
-      expect(wrapper.contains('Logout')).toBe(false);
-    });
+  it('should open the AboutDialog when the About button is clicked', () => {
+    const wrapper = shallow(<Menu />);
+    expect(wrapper.find(AboutDialog).prop('open')).toBe(false);
+    wrapper
+      .find(MenuItem)
+      .at(0)
+      .simulate('click');
+    expect(wrapper.find(AboutDialog).prop('open')).toBe(true);
+  });
+
+  it('should open the SettingsDialog when the Settings button is clicked', () => {
+    const wrapper = shallow(<Menu />);
+    expect(wrapper.find(SettingsDialog).prop('open')).toBe(false);
+    wrapper
+      .find(MenuItem)
+      .at(1)
+      .simulate('click');
+    expect(wrapper.find(SettingsDialog).prop('open')).toBe(true);
   });
 });
