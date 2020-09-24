fdbc95263a69223aec7ece5348ba0547f9a143d8
Tim Mendoza <tmendoza@twilio.com>
2020-09-24 16:07:24 -0600

    Fix menu tests

--- src/components/MenuBar/Menu/Menu.test.tsx
+++ src/components/MenuBar/Menu/Menu.test.tsx
@@ -1,36 +1,66 @@
 import React from 'react';
 import AboutDialog from '../AboutDialog/AboutDialog';
 import { Button, MenuItem } from '@material-ui/core';
+import DeviceSelector from '../DeviceSelector/DeviceSelector';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Menu from './Menu';
 import MenuContainer from '@material-ui/core/Menu';
-import SettingsDialog from '../SettingsDialog/SettingsDialog';
+import MoreIcon from '@material-ui/icons/MoreVert';
 import { shallow } from 'enzyme';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+
+jest.mock('@material-ui/core/useMediaQuery');
+const mockUseMediaQuery = useMediaQuery as jest.Mock<boolean>;
 
 describe('the Menu component', () => {
-  it('should open the Menu when the Button is clicked', () => {
-    const wrapper = shallow(<Menu />);
-    expect(wrapper.find(MenuContainer).prop('open')).toBe(false);
-    wrapper.find(Button).simulate('click');
-    expect(wrapper.find(MenuContainer).prop('open')).toBe(true);
+  describe('on desktop devices', () => {
+    beforeAll(() => {
+      mockUseMediaQuery.mockImplementation(() => false);
+    });
+
+    it('should open the Menu when the Button is clicked', () => {
+      const wrapper = shallow(<Menu />);
+      expect(wrapper.find(MenuContainer).prop('open')).toBe(false);
+      wrapper.find(Button).simulate('click');
+      expect(wrapper.find(MenuContainer).prop('open')).toBe(true);
+    });
+
+    it('should open the AboutDialog when the About button is clicked', () => {
+      const wrapper = shallow(<Menu />);
+      expect(wrapper.find(AboutDialog).prop('open')).toBe(false);
+      wrapper
+        .find(MenuItem)
+        .at(0)
+        .simulate('click');
+      expect(wrapper.find(AboutDialog).prop('open')).toBe(true);
+    });
+
+    it('should open the DeviceSelector when the Settings button is clicked', () => {
+      const wrapper = shallow(<Menu />);
+      expect(wrapper.find(DeviceSelector).prop('open')).toBe(false);
+      wrapper
+        .find(MenuItem)
+        .at(1)
+        .simulate('click');
+      expect(wrapper.find(DeviceSelector).prop('open')).toBe(true);
+    });
+
+    it('should render the correct icon', () => {
+      const wrapper = shallow(<Menu />);
+      expect(wrapper.find(ExpandMoreIcon).exists()).toBe(true);
+      expect(wrapper.find(MoreIcon).exists()).toBe(false);
+    });
   });
 
-  it('should open the AboutDialog when the About button is clicked', () => {
-    const wrapper = shallow(<Menu />);
-    expect(wrapper.find(AboutDialog).prop('open')).toBe(false);
-    wrapper
-      .find(MenuItem)
-      .at(0)
-      .simulate('click');
-    expect(wrapper.find(AboutDialog).prop('open')).toBe(true);
-  });
+  describe('on mobile devices', () => {
+    beforeAll(() => {
+      mockUseMediaQuery.mockImplementation(() => true);
+    });
 
-  it('should open the SettingsDialog when the Settings button is clicked', () => {
-    const wrapper = shallow(<Menu />);
-    expect(wrapper.find(SettingsDialog).prop('open')).toBe(false);
-    wrapper
-      .find(MenuItem)
-      .at(1)
-      .simulate('click');
-    expect(wrapper.find(SettingsDialog).prop('open')).toBe(true);
+    it('should render the correct icon', () => {
+      const wrapper = shallow(<Menu />);
+      expect(wrapper.find(ExpandMoreIcon).exists()).toBe(false);
+      expect(wrapper.find(MoreIcon).exists()).toBe(true);
+    });
   });
 });
