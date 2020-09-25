877f442ba09e80df1df39c82fee7e4d0ba2cd3ad
Tim Mendoza <tmendoza@twilio.com>
2020-09-24 18:47:27 -0600

    Fix getLocalTracks tests

--- src/components/VideoProvider/useLocalTracks/useLocalTracks.test.tsx
+++ src/components/VideoProvider/useLocalTracks/useLocalTracks.test.tsx
@@ -1,38 +1,109 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import useLocalTracks from './useLocalTracks';
 import Video from 'twilio-video';
-import { EventEmitter } from 'events';
+import { useHasAudioInputDevices, useHasVideoInputDevices } from '../../../hooks/deviceHooks/deviceHooks';
+
+jest.mock('../../../hooks/deviceHooks/deviceHooks');
+const mockUseHasVideoInputDevices = useHasVideoInputDevices as jest.Mock<any>;
+const mockUseHasAudioInputDevices = useHasAudioInputDevices as jest.Mock<any>;
+
+mockUseHasAudioInputDevices.mockImplementation(() => true);
+mockUseHasVideoInputDevices.mockImplementation(() => true);
 
 describe('the useLocalTracks hook', () => {
   afterEach(jest.clearAllMocks);
 
-  it('should return an array of tracks and two functions', async () => {
-    const { result, waitForNextUpdate } = renderHook(useLocalTracks);
-    expect(result.current.localTracks).toEqual([]);
-    await waitForNextUpdate();
-    expect(result.current.localTracks).toEqual([expect.any(EventEmitter), expect.any(EventEmitter)]);
-    expect(result.current.getLocalVideoTrack).toEqual(expect.any(Function));
-  });
+  describe('the getAudioAndVideoTracks function', () => {
+    it('should create local audio and video tracks', async () => {
+      Date.now = () => 123456;
+      const { result, waitForNextUpdate } = renderHook(useLocalTracks);
 
-  it('should create local tracks when loaded', async () => {
-    Date.now = () => 123456;
-    const { waitForNextUpdate } = renderHook(useLocalTracks);
-    await waitForNextUpdate();
-    expect(Video.createLocalTracks).toHaveBeenCalledWith({
-      audio: true,
-      video: {
-        frameRate: 24,
-        width: 1280,
-        height: 720,
-        name: 'camera-123456',
-      },
+      await act(async () => {
+        result.current.getAudioAndVideoTracks();
+        await waitForNextUpdate();
+      });
+
+      expect(Video.createLocalTracks).toHaveBeenCalledWith({
+        audio: true,
+        video: {
+          frameRate: 24,
+          width: 1280,
+          height: 720,
+          name: 'camera-123456',
+        },
+      });
+    });
+
+    it('should create a local audio track when no video devices are present', async () => {
+      mockUseHasVideoInputDevices.mockImplementationOnce(() => false);
+
+      const { result, waitForNextUpdate } = renderHook(useLocalTracks);
+
+      await act(async () => {
+        result.current.getAudioAndVideoTracks();
+        await waitForNextUpdate();
+      });
+
+      expect(Video.createLocalTracks).toHaveBeenCalledWith({
+        audio: true,
+        video: false,
+      });
+    });
+
+    it('should create a local video track when no audio devices are present', async () => {
+      mockUseHasAudioInputDevices.mockImplementationOnce(() => false);
+
+      const { result, waitForNextUpdate } = renderHook(useLocalTracks);
+
+      await act(async () => {
+        result.current.getAudioAndVideoTracks();
+        await waitForNextUpdate();
+      });
+
+      expect(Video.createLocalTracks).toHaveBeenCalledWith({
+        audio: false,
+        video: {
+          frameRate: 24,
+          width: 1280,
+          height: 720,
+          name: 'camera-123456',
+        },
+      });
+    });
+
+    it('should not create any tracks when no input devices are present', async () => {
+      mockUseHasAudioInputDevices.mockImplementationOnce(() => false);
+      mockUseHasVideoInputDevices.mockImplementationOnce(() => false);
+
+      const { result } = renderHook(useLocalTracks);
+
+      await result.current.getAudioAndVideoTracks();
+
+      expect(Video.createLocalTracks).not.toHaveBeenCalled();
+    });
+
+    it('should return an error when there is an error creating a track', async () => {
+      (Video.createLocalTracks as jest.Mock<any>).mockImplementationOnce(() => Promise.reject('testError'));
+      const { result, waitForNextUpdate } = renderHook(useLocalTracks);
+
+      act(() => {
+        expect(result.current.getAudioAndVideoTracks()).rejects.toBe('testError');
+      });
+
+      await waitForNextUpdate();
     });
   });
 
   describe('the removeLocalVideoTrack function', () => {
     it('should call videoTrack.stop() and remove the videoTrack from state', async () => {
       const { result, waitForNextUpdate } = renderHook(useLocalTracks);
-      await waitForNextUpdate();
+
+      // First, get tracks
+      await act(async () => {
+        result.current.getAudioAndVideoTracks();
+        await waitForNextUpdate();
+      });
+
       const initialVideoTrack = result.current.localTracks.find(track => track.kind === 'video');
       expect(initialVideoTrack!.stop).not.toHaveBeenCalled();
       expect(initialVideoTrack).toBeTruthy();
