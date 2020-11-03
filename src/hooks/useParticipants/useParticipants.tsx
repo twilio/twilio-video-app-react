01524095cb71543d6ed24fe106cf84ae86d2548d
giladadon1997 <44743270+giladadon1997@users.noreply.github.com>
2020-11-03 04:36:20 -0800

    VA-376-Video-of-each-participant-needs-to-stay-static-UNLESS-a-participant-pins-the-main-speaker-in-options (#15)
    * fix
    
    * use
    
    * dominantSpeaker

--- src/hooks/useParticipants/useParticipants.tsx
+++ src/hooks/useParticipants/useParticipants.tsx
@@ -6,31 +6,28 @@ import useVideoContext from '../useVideoContext/useVideoContext';
 export default function useParticipants() {
   const { room } = useVideoContext();
   const dominantSpeaker = useDominantSpeaker();
-    const [participants, setParticipants] = useState(
-        Array.from(room.participants.values()));
+  const [useDominantSpeakerEffect] = useState(false);
+  const [participants, setParticipants] = useState(Array.from(room.participants.values()));
 
   // When the dominant speaker changes, they are moved to the front of the participants array.
   // This means that the most recent dominant speakers will always be near the top of the
   // ParticipantStrip component.
+
   useEffect(() => {
-    if (dominantSpeaker) {
+    if (dominantSpeaker && useDominantSpeakerEffect) {
+      console.log(useDominantSpeakerEffect);
       setParticipants(prevParticipants => [
         dominantSpeaker,
-          ...prevParticipants.filter(
-              participant => participant !== dominantSpeaker
-          ),
+        ...prevParticipants.filter(participant => participant !== dominantSpeaker),
       ]);
     }
-  },
-      [dominantSpeaker]);
+  }, [dominantSpeaker, useDominantSpeakerEffect]);
 
   useEffect(() => {
     const participantConnected = (participant: RemoteParticipant) =>
       setParticipants(prevParticipants => [...prevParticipants, participant]);
     const participantDisconnected = (participant: RemoteParticipant) =>
-        setParticipants(prevParticipants =>
-            prevParticipants.filter(p => p !== participant)
-        );
+      setParticipants(prevParticipants => prevParticipants.filter(p => p !== participant));
     room.on('participantConnected', participantConnected);
     room.on('participantDisconnected', participantDisconnected);
     return () => {
