63a56bab586d0deea79146d9acb7af3654739ad5
jeferbc <jeffe.bernal@gmail.com>
2020-11-05 12:26:03 -0500

    remove ideal key for constaints. NotReadable error continue with ideal key

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_VIDEO_CONSTRAINTS: MediaStreamConstraints['video'] = {
-  width: { min: 640, max: 1280, ideal: 1280 },
-  height: { min: 480, max: 720, ideal: 720 },
+  width: { min: 640, max: 1280 },
+  height: { min: 480, max: 720 },
   frameRate: 24,
 };
