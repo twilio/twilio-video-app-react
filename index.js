bbbfd596e327573a5439223ba0175705bdf632af
Mike <mike@mikezorn.com>
2020-11-05 21:34:46 -0500

    Match react version with web app

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-import { VideoApp } from './dist/twilio-video-app-react';
+import VideoApp from './dist/twilio-video-app-react';
 
 // this file loads the module in consuming web app
 export default VideoApp;
