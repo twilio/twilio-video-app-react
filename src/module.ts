5cafe484dfbd63ff9eaceaf5575c1897e0f4c2b6
Mike <mike@mikezorn.com>
2020-11-06 11:41:27 -0500

    include the theme in package response

--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,11 @@
 'use strict';
 import AppStateProvider from './state';
 import VideoApp from './VideoApp';
+import theme from './theme';
 
 // this file builds the dist package
 export default {
   VideoApp,
   AppStateProvider,
+  theme,
 };
