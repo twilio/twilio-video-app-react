e0a4745a574c7650eb05e19956910ec60be62b59
Tim Mendoza <tmendoza@twilio.com>
2020-12-04 10:00:35 -0700

    Add icons and snackbar to Menu component

--- src/components/MenuBar/Menu/Menu.tsx
+++ src/components/MenuBar/Menu/Menu.tsx
@@ -3,18 +3,26 @@ import AboutDialog from '../../AboutDialog/AboutDialog';
 import Button from '@material-ui/core/Button';
 import DeviceSelectionDialog from '../../DeviceSelectionDialog/DeviceSelectionDialog';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import InfoIcon from '../../../icons/InfoIcon';
+import { Link, styled, Theme, useMediaQuery } from '@material-ui/core';
 import MenuContainer from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MoreIcon from '@material-ui/icons/MoreVert';
-import RecordingButton from '../RecordingButton/RecordingButton';
+import RecordingButton from './RecordingButton/RecordingButton';
+import SettingsIcon from '../../../icons/SettingsIcon';
+import Snackbar from '../../Snackbar/Snackbar';
 import Typography from '@material-ui/core/Typography';
-import { Theme, useMediaQuery } from '@material-ui/core';
+
+export const IconContainer = styled('div')({
+  width: '1em',
+});
 
 export default function Menu(props: { buttonClassName?: string }) {
   const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('sm'));
   const [aboutOpen, setAboutOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
   const [settingsOpen, setSettingsOpen] = useState(false);
+  const [isRecordingSnackbarOpen, setIsRecordingSnackbarOpen] = useState(false);
 
   const anchorRef = useRef<HTMLButtonElement>(null);
 
@@ -44,12 +52,18 @@ export default function Menu(props: { buttonClassName?: string }) {
         }}
       >
         <MenuItem onClick={() => setAboutOpen(true)}>
+          <IconContainer>
+            <InfoIcon color="#707578" />
+          </IconContainer>
           <Typography variant="body1">About</Typography>
         </MenuItem>
         <MenuItem onClick={() => setSettingsOpen(true)}>
-          <Typography variant="body1">Audio and Video Settings</Typography>
+          <IconContainer>
+            <SettingsIcon />
+          </IconContainer>
+          <Typography variant="body1">Conference Settings</Typography>
         </MenuItem>
-        <RecordingButton />
+        <RecordingButton onClick={() => setMenuOpen(false)} setIsRecordingSnackbarOpen={setIsRecordingSnackbarOpen} />
       </MenuContainer>
       <AboutDialog
         open={aboutOpen}
@@ -65,6 +79,21 @@ export default function Menu(props: { buttonClassName?: string }) {
           setMenuOpen(false);
         }}
       />
+      <Snackbar
+        open={isRecordingSnackbarOpen}
+        headline="Recording Complete"
+        message={
+          <>
+            You can view the recording in the{' '}
+            <Link target="_blank" rel="noopener" href="https://www.twilio.com/console/video/logs/recordings">
+              Twilio Console
+            </Link>
+            . Recordings will be available once this room has ended.
+          </>
+        }
+        variant="info"
+        handleClose={() => setIsRecordingSnackbarOpen(false)}
+      ></Snackbar>
     </>
   );
 }
