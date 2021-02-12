2c22fa53313fcbb1fbd09e55fabf27ee508a4a40
Olivia Pyskoty <opyskoty@twilio.com>
2021-02-12 16:46:16 -0500

    add button for closing chat window

---
+++ src/components/Buttons/CloseChatWindowButton/CloseChatWindowButton.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+
+import CloseChatWindowIcon from '../../../icons/CloseChatWindowIcon';
+
+import useChatContext from '../../../hooks/useChatContext/useChatContext';
+
+export default function CloseChatWindowButton() {
+  const { setIsChatWindowOpen } = useChatContext();
+
+  const closeChatWindow = () => {
+    setIsChatWindowOpen(false);
+  };
+
+  return (
+    <Button onClick={closeChatWindow}>
+      <CloseChatWindowIcon />
+    </Button>
+  );
+}
