b696528c0ac92c4103871ae88658b934e9bbb4e0
Olivia Pyskoty <opyskoty@twilio.com>
2021-02-18 17:57:03 -0500

    remove snapshot tests from Room tests

--- src/components/Room/Room.test.tsx
+++ src/components/Room/Room.test.tsx
@@ -13,13 +13,12 @@ mockUseChatContext.mockImplementation(() => ({ setIsChatWindowOpen: mockToggleCh
 describe('the Room component', () => {
   it('should render correctly with chat window closed', () => {
     const wrapper = shallow(<Room />);
-    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.prop('className')).not.toContain('chatWindowOpen');
   });
 
   it('should render correctly with chat window open', () => {
     mockUseChatContext.mockImplementationOnce(() => ({ isChatWindowOpen: true }));
     const wrapper = shallow(<Room />);
-    expect(wrapper).toMatchSnapshot();
     expect(wrapper.prop('className')).toContain('chatWindowOpen');
   });
 });
