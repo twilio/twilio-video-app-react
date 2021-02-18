e380e46d28053d965660df29bcc0ea76c59347a3
Olivia Pyskoty <opyskoty@twilio.com>
2021-02-18 17:54:01 -0500

    rename icon to CloseChat icon

--- src/icons/CloseIcon.tsx
+++ src/icons/CloseIcon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function CloseChatWindowIcon() {
+export default function CloseIcon() {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
