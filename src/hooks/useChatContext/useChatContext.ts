4f5617a75ba38e26a231694af9bf2ad59cb690e9
Olivia Pyskoty <opyskoty@twilio.com>
2021-02-18 17:56:15 -0500

    fix typo to usechatContext

--- src/hooks/useChatContext/useChatContext.ts
+++ src/hooks/useChatContext/useChatContext.ts
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { ChatContext } from '../../components/ChatProvider';
 
-export default function useVideoContext() {
+export default function useChatContext() {
   const context = useContext(ChatContext);
   if (!context) {
     throw new Error('useChatContext must be used within a ChatProvider');
