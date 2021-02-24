9541a3fb81110a1abca19f016977b88e0c46484c
Tim Mendoza <tmendoza@twilio.com>
2021-02-23 20:22:14 -0700

    Update hooks in chat provider

--- src/components/ChatProvider/index.tsx
+++ src/components/ChatProvider/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useRef, useState } from 'react';
+import React, { createContext, useCallback, useEffect, useRef, useState } from 'react';
 import { Client } from '@twilio/conversations';
 import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
 import { Conversation } from '@twilio/conversations/lib/conversation';
@@ -11,6 +11,7 @@ type ChatContextType = {
   connect: (token: string) => void;
   hasUnreadMessages: boolean;
   messages: Message[];
+  conversation?: Conversation;
 };
 
 export const ChatContext = createContext<ChatContextType>(null!);
@@ -29,11 +30,17 @@ export function ChatProvider({ children }: ChatProviderProps) {
   const [hasUnreadMessages, setHasUnreadMessages] = useState(false);
   const [chatClient, setChatClient] = useState<Client>();
 
+  const connect = useCallback((token: string) => {
+    Client.create(token).then(client => {
+      //@ts-ignore
+      window.chatClient = client;
+      setChatClient(client);
+    });
+  }, []);
+
   useEffect(() => {
-    const handleMessageAdded = (message: Message) => {
-      setMessages(oldMessages => [...oldMessages, message]);
-      if (!isChatWindowOpenRef.current) setHasUnreadMessages(true);
-    };
+    const handleMessageAdded = (message: Message) => setMessages(oldMessages => [...oldMessages, message]);
+
     if (conversation) {
       conversation.getMessages().then(messages => setMessages(messages.items));
       conversation.on('messageAdded', handleMessageAdded);
@@ -44,30 +51,30 @@ export function ChatProvider({ children }: ChatProviderProps) {
   }, [conversation]);
 
   useEffect(() => {
+    if (!isChatWindowOpenRef.current) {
+      setHasUnreadMessages(true);
+    }
+  }, [messages]);
+
+  useEffect(() => {
     isChatWindowOpenRef.current = isChatWindowOpen;
     if (isChatWindowOpen) setHasUnreadMessages(false);
   }, [isChatWindowOpen]);
 
-  function connect(token: string) {
-    Client.create(token).then(client => {
-      //@ts-ignore
-      window.chatClient = client;
-      setChatClient(client);
-    });
-  }
-
   useEffect(() => {
-    if (state === 'connected' && chatClient) {
+    if (room && chatClient) {
       chatClient.getConversationByUniqueName(room.sid).then(conversation => {
         //@ts-ignore
         window.chatConversation = conversation;
         setConversation(conversation);
       });
     }
-  }, [state, chatClient]);
+  }, [room, chatClient]);
 
   return (
-    <ChatContext.Provider value={{ isChatWindowOpen, setIsChatWindowOpen, connect, hasUnreadMessages, messages }}>
+    <ChatContext.Provider
+      value={{ isChatWindowOpen, setIsChatWindowOpen, connect, hasUnreadMessages, messages, conversation }}
+    >
       {children}
     </ChatContext.Provider>
   );
