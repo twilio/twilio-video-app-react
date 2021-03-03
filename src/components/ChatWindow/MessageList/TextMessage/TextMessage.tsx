84794ca0f9b705349931f96dbcce27fcf7aafe7a
olipyskoty <77076398+olipyskoty@users.noreply.github.com>
2021-03-02 19:28:29 -0500

    remove unnecessary material-ui methods
    Co-authored-by: timmydoza <tmendoza@twilio.com>

--- src/components/ChatWindow/MessageList/TextMessage/TextMessage.tsx
+++ src/components/ChatWindow/MessageList/TextMessage/TextMessage.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    messageContainer: {
-      borderRadius: '16px',
-      display: 'inline-flex',
-      alignItems: 'center',
-      padding: '0.5em 0.8em 0.6em',
-      margin: '0.3em 0 0',
-      wordBreak: 'break-word',
-      backgroundColor: '#E1E3EA',
-      hyphens: 'auto',
-    },
-    isLocalParticipant: {
-      backgroundColor: '#CCE4FF',
-    },
-  })
-);
+const useStyles = makeStyles({
+  messageContainer: {
+    borderRadius: '16px',
+    display: 'inline-flex',
+    alignItems: 'center',
+    padding: '0.5em 0.8em 0.6em',
+    margin: '0.3em 0 0',
+    wordBreak: 'break-word',
+    backgroundColor: '#E1E3EA',
+    hyphens: 'auto',
+  },
+  isLocalParticipant: {
+    backgroundColor: '#CCE4FF',
+  },
+});
 
 interface TextMessageProps {
   body: string;
