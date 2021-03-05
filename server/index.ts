53ad2abb2a30f540dadc010dbbc3636d1756a6f8
Tim Mendoza <tmendoza@twilio.com>
2021-03-05 14:12:59 -0700

    Temorarily remove firebaseAuthMiddleware and recording rules route

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import './bootstrap-globals';
 import { createExpressHandler } from './createExpressHandler';
-import express, { RequestHandler } from 'express';
+import express from 'express';
 import path from 'path';
 import { ServerlessFunction } from './types';
 
@@ -9,18 +9,11 @@ const PORT = process.env.PORT ?? 8081;
 const app = express();
 app.use(express.json());
 
-const noopMiddleware: RequestHandler = (_, __, next) => next();
-const authMiddleware =
-  process.env.REACT_APP_SET_AUTH === 'firebase' ? require('./firebaseAuthMiddleware') : noopMiddleware;
-
 const tokenEndpoint: ServerlessFunction = require('@twilio-labs/plugin-rtc/src/serverless/functions/token').handler;
-const recordingRulesEndpoint: ServerlessFunction = require('@twilio-labs/plugin-rtc/src/serverless/functions/recordingrules')
-  .handler;
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.all('/token', authMiddleware, createExpressHandler(tokenEndpoint));
-app.all('/recordingrules', authMiddleware, createExpressHandler(recordingRulesEndpoint));
+app.all('/token', createExpressHandler(tokenEndpoint));
 
 app.get('*', (_, res) => res.sendFile(path.join(__dirname, '../build/index.html')));
 
