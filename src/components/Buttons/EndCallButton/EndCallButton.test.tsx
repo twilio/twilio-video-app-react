e49580e55936d2f66bacf1bef77bfcac5c24573f
Tim Mendoza <tmendoza@twilio.com>
2021-03-10 09:25:41 -0700

    Update end call button to only disconnect from room

--- src/components/Buttons/EndCallButton/EndCallButton.test.tsx
+++ src/components/Buttons/EndCallButton/EndCallButton.test.tsx
@@ -7,35 +7,14 @@ const mockVideoContext = {
   room: {
     disconnect: jest.fn(),
   },
-  isSharingScreen: false,
-  removeLocalAudioTrack: jest.fn(),
-  removeLocalVideoTrack: jest.fn(),
 };
 
 jest.mock('../../../hooks/useVideoContext/useVideoContext', () => () => mockVideoContext);
 
 describe('End Call button', () => {
-  describe('when it is clicked', () => {
-    describe('while sharing screen', () => {
-      let wrapper;
-
-      beforeAll(() => {
-        jest.clearAllMocks();
-        wrapper = shallow(<EndCallButton />);
-        wrapper.simulate('click');
-      });
-
-      it('should stop local audio tracks', () => {
-        expect(mockVideoContext.removeLocalAudioTrack).toHaveBeenCalled();
-      });
-
-      it('should stop local video tracks', () => {
-        expect(mockVideoContext.removeLocalVideoTrack).toHaveBeenCalled();
-      });
-
-      it('should disconnect from the room ', () => {
-        expect(mockVideoContext.room.disconnect).toHaveBeenCalled();
-      });
-    });
+  it('should disconnect from the room when clicked', () => {
+    const wrapper = shallow(<EndCallButton />);
+    wrapper.simulate('click');
+    expect(mockVideoContext.room.disconnect).toHaveBeenCalled();
   });
 });
