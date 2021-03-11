91cb5062b14d435dbf04185237658114f46d72fb
Tim Mendoza <tmendoza@twilio.com>
2021-03-10 21:25:55 -0700

    Add firebaseAuthMiddleware

---
+++ server/firebaseAuthMiddleware.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from 'express';
+import firebaseAdmin from 'firebase-admin';
+
+firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(require('./serviceAccountKey.json')),
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+});
+
+const verifyIdToken = firebaseAdmin.auth().verifyIdToken;
+
+const firebaseAuthMiddleware: RequestHandler = async (req, res, next) => {
+  const authHeader = req.get('authorization');
+
+  if (!authHeader) {
+    return res.status(401).send();
+  }
+
+  try {
+    const token = await verifyIdToken(authHeader);
+    if (token.email && /@twilio.com$/.test(token.email)) {
+      next();
+    } else {
+      res.status(401).send();
+    }
+  } catch {
+    res.status(401).send();
+  }
+};
+export default module.exports = firebaseAuthMiddleware;
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import './bootstrap-globals';
 import { createExpressHandler } from './createExpressHandler';
-import express from 'express';
+import express, { RequestHandler } from 'express';
 import path from 'path';
 import { ServerlessFunction } from './types';
 
@@ -9,11 +9,16 @@ const PORT = process.env.PORT ?? 8081;
 const app = express();
 app.use(express.json());
 
-const tokenEndpoint: ServerlessFunction = require('@twilio-labs/plugin-rtc/src/serverless/functions/token').handler;
+const tokenFunction: ServerlessFunction = require('@twilio-labs/plugin-rtc/src/serverless/functions/token').handler;
+const tokenEndpoint = createExpressHandler(tokenFunction);
+
+const noopMiddleware: RequestHandler = (_, __, next) => next();
+const authMiddleware =
+  process.env.REACT_APP_SET_AUTH === 'firebase' ? require('./firebaseAuthMiddleware') : noopMiddleware;
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.all('/token', createExpressHandler(tokenEndpoint));
+app.all('/token', authMiddleware, tokenEndpoint);
 
 app.get('*', (_, res) => res.sendFile(path.join(__dirname, '../build/index.html')));
 
