9262ef12f1726e9332ae8c4f5357a65377ee1632
Tim Mendoza <tmendoza@twilio.com>
2021-03-11 13:33:07 -0700

    Add tests for ToggleChatButton

--- src/components/Buttons/ToggleChatButton/ToggleChatButton.test.tsx
+++ src/components/Buttons/ToggleChatButton/ToggleChatButton.test.tsx
@@ -1,27 +1,119 @@
 import React from 'react';
-import { shallow } from 'enzyme';
 import { Button } from '@material-ui/core';
+import { EventEmitter } from 'events';
+import { shallow, mount } from 'enzyme';
 
 import ChatIcon from '../../../icons/ChatIcon';
-import ToggleChatButton from './ToggleChatButton';
+import ToggleChatButton, { ANIMATION_DURATION } from './ToggleChatButton';
 import useChatContext from '../../../hooks/useChatContext/useChatContext';
+import { act } from 'react-dom/test-utils';
 
 jest.mock('../../../hooks/useChatContext/useChatContext');
 const mockUseChatContext = useChatContext as jest.Mock<any>;
 
+const mockConversation = new EventEmitter();
 const mockToggleChatWindow = jest.fn();
-mockUseChatContext.mockImplementation(() => ({ setIsChatWindowOpen: mockToggleChatWindow, isChatWindowOpen: false }));
+mockUseChatContext.mockImplementation(() => ({
+  setIsChatWindowOpen: mockToggleChatWindow,
+  isChatWindowOpen: false,
+  conversation: mockConversation,
+}));
 
 describe('the ToggleChatButton component', () => {
-  it('should render correctly when chat is enabled', () => {
+  it('should be enabled when a conversation is present', () => {
     const wrapper = shallow(<ToggleChatButton />);
-    expect(wrapper.prop('startIcon')).toEqual(<ChatIcon />);
-    expect(wrapper.text()).toBe('Chat');
+    expect(wrapper.prop('disabled')).toBe(false);
+  });
+
+  it('should be disabled when a conversation is not present', () => {
+    mockUseChatContext.mockImplementationOnce(() => ({
+      setIsChatWindowOpen: mockToggleChatWindow,
+      isChatWindowOpen: false,
+      conversation: null,
+    }));
+    const wrapper = shallow(<ToggleChatButton />);
+    expect(wrapper.prop('disabled')).toBe(true);
   });
 
   it('should call the correct toggle function when clicked', () => {
     const wrapper = shallow(<ToggleChatButton />);
     wrapper.find(Button).simulate('click');
     expect(mockToggleChatWindow).toHaveBeenCalledWith(true);
+  });
+
+  it('should show an indicator when there are unread messages', () => {
+    mockUseChatContext.mockImplementationOnce(() => ({
+      setIsChatWindowOpen: mockToggleChatWindow,
+      isChatWindowOpen: false,
+      conversation: mockConversation,
+      hasUnreadMessages: true,
+    }));
+
+    const wrapper = mount(<ToggleChatButton />);
+    const notificationCircle = wrapper.findWhere(node => node.prop('className')?.includes('circle'));
+    expect(notificationCircle.prop('className')).toContain('hasUnreadMessages');
+  });
+
+  it('should not show an indicator when there are no unread messages', () => {
+    mockUseChatContext.mockImplementationOnce(() => ({
+      setIsChatWindowOpen: mockToggleChatWindow,
+      isChatWindowOpen: false,
+      conversation: mockConversation,
+      hasUnreadMessages: false,
+    }));
+
+    const wrapper = mount(<ToggleChatButton />);
+    const notificationCircle = wrapper.findWhere(node => node.prop('className')?.includes('circle'));
+    expect(notificationCircle.prop('className')).not.toContain('hasUnreadMessages');
+  });
+
+  it(`should add the 'animate' class for ${ANIMATION_DURATION}ms when a new message is received when the chat window is closed`, () => {
+    jest.useFakeTimers();
+    mockUseChatContext.mockImplementationOnce(() => ({
+      setIsChatWindowOpen: mockToggleChatWindow,
+      isChatWindowOpen: false,
+      conversation: mockConversation,
+    }));
+
+    const wrapper = mount(<ToggleChatButton />);
+    let notificationRing = wrapper.findWhere(node => node.prop('className')?.includes('ring'));
+    expect(notificationRing.prop('className')).not.toContain('animate');
+
+    act(() => {
+      mockConversation.emit('messageAdded');
+    });
+    wrapper.update();
+
+    notificationRing = wrapper.findWhere(node => node.prop('className')?.includes('ring'));
+    expect(notificationRing.prop('className')).toContain('animate');
+
+    act(() => {
+      jest.advanceTimersByTime(ANIMATION_DURATION);
+    });
+    wrapper.update();
+
+    notificationRing = wrapper.findWhere(node => node.prop('className')?.includes('ring'));
+    expect(notificationRing.prop('className')).not.toContain('animate');
+  });
+
+  it(`should not add the 'animate' class when a new message is received when the chat window is open`, () => {
+    jest.useFakeTimers();
+    mockUseChatContext.mockImplementationOnce(() => ({
+      setIsChatWindowOpen: mockToggleChatWindow,
+      isChatWindowOpen: open,
+      conversation: mockConversation,
+    }));
+
+    const wrapper = mount(<ToggleChatButton />);
+    let notificationRing = wrapper.findWhere(node => node.prop('className')?.includes('ring'));
+    expect(notificationRing.prop('className')).not.toContain('animate');
+
+    act(() => {
+      mockConversation.emit('messageAdded');
+    });
+    wrapper.update();
+
+    notificationRing = wrapper.findWhere(node => node.prop('className')?.includes('ring'));
+    expect(notificationRing.prop('className')).not.toContain('animate');
   });
 });
