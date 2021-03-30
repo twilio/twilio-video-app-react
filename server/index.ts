149049474b74730f5ddb03fbba574e2f0a3ac7ad
Tim Mendoza <tmendoza@twilio.com>
2021-03-30 11:22:58 -0600

    Add comments to the server

--- server/index.ts
+++ server/index.ts
@@ -9,6 +9,8 @@ const PORT = process.env.PORT ?? 8081;
 const app = express();
 app.use(express.json());
 
+// This server reuses the serverless endpoints from the "plugin-rtc" Twilio CLI Plugin, which is used when the "npm run deploy:twilio-cli" command is run.
+// The documentation for this endpoint can be found in the README file here: https://github.com/twilio-labs/plugin-rtc
 const tokenFunction: ServerlessFunction = require('@twilio-labs/plugin-rtc/src/serverless/functions/token').handler;
 const tokenEndpoint = createExpressHandler(tokenFunction);
 
@@ -19,6 +21,8 @@ const authMiddleware =
 app.all('/token', authMiddleware, tokenEndpoint);
 
 app.use((req, res, next) => {
+  // Here we add Cache-Control headers in accordance with the create-react-app best practices.
+  // See: https://create-react-app.dev/docs/production-build/#static-file-caching
   if (req.path === '/' || req.path === 'index.html') {
     res.set('Cache-Control', 'no-cache');
   } else {
@@ -29,6 +33,7 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('*', (_, res) => {
+  // Don't cache index.html
   res.set('Cache-Control', 'no-cache');
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
