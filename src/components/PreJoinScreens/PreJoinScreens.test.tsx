20a0e7d566f0678a0ea58adb08ff6b8fad12d565
Olivia Pyskoty <opyskoty@twilio.com>
2021-04-12 17:13:37 -0400

    remove PreflightTest from PreJoinScreens tests

--- src/components/PreJoinScreens/PreJoinScreens.test.tsx
+++ src/components/PreJoinScreens/PreJoinScreens.test.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { act } from 'react-dom/test-utils';
 import DeviceSelectionScreen from './DeviceSelectionScreen/DeviceSelectionScreen';
-import MediaErrorSnackbar from './MediaErrorSnackbar/MediaErrorSnackbar';
 import { mount, shallow } from 'enzyme';
-import PreflightTest from './PreflightTest/PreflightTest';
 import PreJoinScreens from './PreJoinScreens';
 import RoomNameScreen from './RoomNameScreen/RoomNameScreen';
 import { useParams } from 'react-router-dom';
 import { useAppState } from '../../state';
 import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
-import Video from 'twilio-video';
 
 delete window.location;
 // @ts-ignore
@@ -80,49 +77,6 @@ describe('the PreJoinScreens component', () => {
     expect(wrapper.find(DeviceSelectionScreen).exists()).toBe(true);
   });
 
-  it('should render the PreflightTest component only while on the DeviceSelection step', () => {
-    const wrapper = shallow(<PreJoinScreens />);
-
-    expect(wrapper.prop('subContent')).toBe(false);
-    expect(wrapper.find(DeviceSelectionScreen).exists()).toBe(false);
-
-    const handleSubmit = wrapper.find(RoomNameScreen).prop('handleSubmit');
-    handleSubmit({ preventDefault: () => {} } as any);
-
-    expect(wrapper.prop('subContent')).toEqual(
-      <>
-        <PreflightTest />
-        <MediaErrorSnackbar />
-      </>
-    );
-    expect(wrapper.find(DeviceSelectionScreen).exists()).toBe(true);
-  });
-
-  it('should not render the PreflightTest component when the Video.testPreflight function does not exist', () => {
-    // Save the testPreflight function
-    const testPreflightFunction = Video.testPreflight;
-
-    // @ts-ignore
-    delete Video.testPreflight;
-    const wrapper = shallow(<PreJoinScreens />);
-
-    expect(wrapper.prop('subContent')).toBe(false);
-    expect(wrapper.find(DeviceSelectionScreen).exists()).toBe(false);
-
-    const handleSubmit = wrapper.find(RoomNameScreen).prop('handleSubmit');
-    handleSubmit({ preventDefault: () => {} } as any);
-
-    expect(wrapper.prop('subContent')).toEqual(
-      <>
-        {undefined}
-        <MediaErrorSnackbar />
-      </>
-    );
-
-    // Restore the testPreflight function to the mock
-    Video.testPreflight = testPreflightFunction;
-  });
-
   it('should populate the room name from the URL and switch to the DeviceSelectionScreen when the displayName is present for the user', () => {
     const wrapper = mount(<PreJoinScreens />);
     const roomName = wrapper.find(DeviceSelectionScreen).prop('roomName');
@@ -154,7 +108,7 @@ describe('the PreJoinScreens component', () => {
       wrapper.update();
     });
 
-    const error = wrapper.children().prop('subContent').props.children[1].props.error;
+    const error = wrapper.children().prop('error');
     expect(error).toBe('testError');
     expect(mockGetAudioAndVideoTracks).toHaveBeenCalledTimes(1); // This makes sure that 'getAudioAndVideoTracks' isn't called repeatedly
   });
