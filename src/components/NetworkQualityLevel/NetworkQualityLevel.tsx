7ffa1439a00240aa3930096965ed9b8e6f554542
Olivia Pyskoty <opyskoty@twilio.com>
2021-04-20 12:45:33 -0400

    add props and styling to NetworkQualityIndicator

--- src/components/NetworkQualityLevel/NetworkQualityLevel.tsx
+++ src/components/NetworkQualityLevel/NetworkQualityLevel.tsx
@@ -1,39 +1,58 @@
 import React from 'react';
-import { styled } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
 import { Participant } from 'twilio-video';
 import useParticipantNetworkQualityLevel from '../../hooks/useParticipantNetworkQualityLevel/useParticipantNetworkQualityLevel';
 
-const Container = styled('div')({
-  display: 'flex',
-  alignItems: 'flex-end',
-  '& div': {
-    width: '2px',
-    marginRight: '1px',
-    '&:not(:last-child)': {
-      borderRight: 'none',
+const useStyles = makeStyles({
+  outerContainer: {
+    width: '28px',
+    height: '28px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: 'rgba(0, 0, 0, 0.5)',
+  },
+  innerContainer: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    '& div': {
+      width: '2px',
+      marginRight: '1px',
+      '&:not(:last-child)': {
+        borderRight: 'none',
+      },
     },
   },
 });
 
+interface NetworkQualityLevelProps {
+  participant: Participant;
+  className?: string;
+}
+
 const STEP = 3;
 const BARS_ARRAY = [0, 1, 2, 3, 4];
 
-export default function NetworkQualityLevel({ participant }: { participant: Participant }) {
+export default function NetworkQualityLevel({ participant, className }: NetworkQualityLevelProps) {
+  const classes = useStyles();
   const networkQualityLevel = useParticipantNetworkQualityLevel(participant);
 
   if (networkQualityLevel === null) return null;
 
   return (
-    <Container>
-      {BARS_ARRAY.map(level => (
-        <div
-          key={level}
-          style={{
-            height: `${STEP * (level + 1)}px`,
-            background: networkQualityLevel > level ? 'white' : 'rgba(255, 255, 255, 0.2)',
-          }}
-        />
-      ))}
-    </Container>
+    <div className={clsx(classes.outerContainer, className)}>
+      <div className={classes.innerContainer}>
+        {BARS_ARRAY.map(level => (
+          <div
+            key={level}
+            style={{
+              height: `${STEP * (level + 1)}px`,
+              background: networkQualityLevel > level ? 'white' : 'rgba(255, 255, 255, 0.2)',
+            }}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
