fd53113b7fa161a18184e17fc2da432a7d699338
Tim Mendoza <tmendoza@twilio.com>
2021-05-06 17:01:16 -0600

    Update a TS definintion

--- src/hooks/useDevices/useDevices.tsx
+++ src/hooks/useDevices/useDevices.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { getDeviceInfo } from '../../utils';
 
 // This returns the type of the value that is returned by a promise resolution
-type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
+type ThenArg<T> = T extends PromiseLike<infer U> ? U : never;
 
 export default function useDevices() {
   const [deviceInfo, setDeviceInfo] = useState<ThenArg<ReturnType<typeof getDeviceInfo>>>({
