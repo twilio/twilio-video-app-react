a4de6cdbaa8172109280aa766a259913bb942fef
Tim Mendoza <tmendoza@twilio.com>
2021-05-05 22:05:54 -0600

    Create getDeviceInfo function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -22,3 +22,15 @@ export function removeUndefineds<T>(obj: T): T {
 
   return target as T;
 }
+
+export async function getDeviceInfo() {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+
+  return {
+    audioInputDevices: devices.filter(device => device.kind === 'audioinput'),
+    videoInputDevices: devices.filter(device => device.kind === 'videoinput'),
+    audioOutputDevices: devices.filter(device => device.kind === 'audiooutput'),
+    hasAudioInputDevices: devices.some(device => device.kind === 'audioinput'),
+    hasVideoInputDevices: devices.some(device => device.kind === 'videoinput'),
+  };
+}
