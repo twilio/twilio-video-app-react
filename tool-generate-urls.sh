d583306d4428f4d2b82e245573d236999fb09f2e
bochoi-twlo <bochoi@twilio.com>
2021-08-17 22:26:13 -0700

    misc changes

--- tool-generate-urls.sh
+++ tool-generate-urls.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 set -e
 
-id=$(whoami)
-defaultRoomName="tele-appointment-${id}"
-read -p "Enter patient's display Name (default ${defaultRoomName}): " roomName
+defaultRoomName="Video Visit Room 0"
+read -p "Enter video room Name (default ${defaultRoomName}): " roomName
 if [[ -z ${roomName} ]]; then
   roomName=${defaultRoomName}
 fi
-echo "roomName: ${roomName}"
 
 defaultProviderName="Dr X"
 read -p "Enter provider's display Name (default ${defaultProviderName}): " providerName
 if [[ -z ${providerName} ]]; then
   providerName=${defaultProviderName}
 fi
-echo "providerName: ${providerName}"
+
 
 defaultPatientName="Jane"
 read -p "Enter patient's display Name (default ${defaultPatientName}): " patientName
 if [[ -z ${patientName} ]]; then
   patientName=${defaultPatientName}
 fi
-echo "patientName: ${patientName}"
+
 
 url=$(twilio rtc:apps:video:view | grep 'Web App URL' | sed -e 's/Web App URL: //')
 echo "url: ${url}"
@@ -32,15 +30,19 @@ echo "host: ${host}"
 passcode=${url#*\?}
 echo "passcode: ${passcode}"
 
+roomName=${roomName// /%20}
+providerName=${providerName// /%20}
+patientName=${patientName// /%20}
+
 echo
-echo "provider link"
+echo "provider link:"
 echo "${host}/?room=${roomName}&persona=provider&name=${providerName}&${passcode}"
 
 echo
-echo "patient link"
+echo "sample patient link:"
 echo "${host}/?room=${roomName}&persona=patient&name=${patientName}&${passcode}"
 
 echo
-echo "3rd party link"
-echo "${host}/?room=${roomName}&persona=thirdparty&name=UncleFrank&${passcode}"
+echo "sample 3rd party link:"
+echo "${host}/?room=${roomName}&persona=thirdparty&name=Uncle%20Frank&${passcode}"
 echo
