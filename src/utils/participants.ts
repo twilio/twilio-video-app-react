513ef44d468c958ee015d087f4e90ec4b9e449a6
leolurch <40560457+leolurch@users.noreply.github.com>
2021-12-01 16:52:09 +0100

    chat window improvements

--- src/utils/participants.ts
+++ src/utils/participants.ts
@@ -26,6 +26,8 @@ export const sortedParticipantsByCategorie = (
   return {
     moderatorParitcipants,
     normalParticipants,
+    // participantCount: allParticipants.length,
+    participantCount: 8,
   };
 };
 
