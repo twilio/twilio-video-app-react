68ff5dc0b42f8fa07627b925c2f928f4e9eb60a5
Tim Mendoza <tmendoza@twilio.com>
2022-03-07 22:28:17 -0700

    Add pagination to GridView component

--- src/components/GridView/GridView.tsx
+++ src/components/GridView/GridView.tsx
@@ -1,32 +1,85 @@
 import React from 'react';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+import ArrowForward from '@material-ui/icons/ArrowForward';
+import clsx from 'clsx';
 import { GRID_MODE_ASPECT_RATIO, GRID_MODE_MARGIN, GRID_MODE_MAX_PARTICIPANTS } from '../../constants';
-import { makeStyles, Theme } from '@material-ui/core';
+import { IconButton, makeStyles } from '@material-ui/core';
+
+import { Pagination } from '@material-ui/lab';
 import Participant from '../Participant/Participant';
 import useGridLayout from '../../hooks/useGridLayout/useGridLayout';
 import useParticipants from '../../hooks/useParticipants/useParticipants';
 import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
+import { usePagination } from '../../hooks/usePagination/usePagination';
 
-const useStyles = makeStyles((theme: Theme) => ({
+const CONTAINER_GUTTER = '50px';
+
+const useStyles = makeStyles({
   container: {
+    position: 'absolute',
     display: 'flex',
-    width: 'calc(100% - 200px)',
+    top: CONTAINER_GUTTER,
+    right: CONTAINER_GUTTER,
+    bottom: CONTAINER_GUTTER,
+    left: CONTAINER_GUTTER,
     margin: '0 auto',
     alignContent: 'center',
     flexWrap: 'wrap',
     justifyContent: 'center',
     gridArea: '1 / 1 / 2 / 3',
   },
-  participant: {
-    '&:nth-child(n + 26)': {
-      display: 'none',
+  buttonContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  buttonContainerLeft: {
+    right: `calc(100% - ${CONTAINER_GUTTER})`,
+    left: 0,
+  },
+  buttonContainerRight: {
+    right: 0,
+    left: `calc(100% - ${CONTAINER_GUTTER})`,
+  },
+  pagination: {
+    '& .MuiPaginationItem-root': {
+      color: 'white',
     },
   },
-}));
+  paginationButton: {
+    color: 'black',
+    background: 'rgba(255, 255, 255, 0.8)',
+    width: '40px',
+    height: '40px',
+    '&:hover': {
+      background: 'rgba(255, 255, 255)',
+    },
+  },
+  paginationContainer: {
+    position: 'absolute',
+    top: `calc(100% - ${CONTAINER_GUTTER})`,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export function GridView() {
   const classes = useStyles();
   const { room } = useVideoContext();
   const participants = useParticipants();
+
+  const { paginatedParticipants, setCurrentPage, currentPage, totalPages } = usePagination([
+    room!.localParticipant,
+    ...participants,
+  ]);
 
   const { participantVideoWidth, containerRef } = useGridLayout(
     Math.min(participants.length + 1, GRID_MODE_MAX_PARTICIPANTS)
@@ -36,22 +89,44 @@ export function GridView() {
   const participantHeight = `${Math.floor(participantVideoWidth * GRID_MODE_ASPECT_RATIO)}px`;
 
   return (
-    <div className={classes.container} ref={containerRef}>
-      <div
-        className={classes.participant}
-        style={{ width: participantWidth, height: participantHeight, margin: GRID_MODE_MARGIN }}
-      >
-        <Participant participant={room!.localParticipant} isLocalParticipant={true} />
+    <>
+      <div className={clsx(classes.buttonContainer, classes.buttonContainerLeft)}>
+        {!(currentPage === 1) && (
+          <IconButton className={classes.paginationButton} onClick={() => setCurrentPage(page => page - 1)}>
+            <ArrowBack />
+          </IconButton>
+        )}
       </div>
-      {participants.map(participant => (
-        <div
-          key={participant.sid}
-          className={classes.participant}
-          style={{ width: participantWidth, height: participantHeight, margin: GRID_MODE_MARGIN }}
-        >
-          <Participant participant={participant} />
-        </div>
-      ))}
-    </div>
+      <div className={clsx(classes.buttonContainer, classes.buttonContainerRight)}>
+        {!(currentPage === totalPages) && (
+          <IconButton className={classes.paginationButton} onClick={() => setCurrentPage(page => page + 1)}>
+            <ArrowForward />
+          </IconButton>
+        )}
+      </div>
+      <div className={classes.paginationContainer}>
+        <Pagination
+          className={classes.pagination}
+          page={currentPage}
+          count={totalPages}
+          onChange={(_, value) => setCurrentPage(value)}
+          shape="rounded"
+          color="primary"
+          size="small"
+          hidePrevButton
+          hideNextButton
+        />
+      </div>
+      <div className={classes.container} ref={containerRef}>
+        {paginatedParticipants.map(participant => (
+          <div
+            key={participant.sid}
+            style={{ width: participantWidth, height: participantHeight, margin: GRID_MODE_MARGIN }}
+          >
+            <Participant participant={participant} isLocalParticipant={participant === room!.localParticipant} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
