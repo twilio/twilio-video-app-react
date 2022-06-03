ae595fc645222c9c06f9a79628b3a50be50b5a05
Tim Mendoza <tmendoza@twilio.com>
2022-06-03 13:45:47 -0600

    Show useful error message when screen sharing is prevented by system permissions

--- src/components/ErrorDialog/enhanceMessage.ts
+++ src/components/ErrorDialog/enhanceMessage.ts
@@ -1,9 +1,11 @@
 // This function is used to provide error messages to the user that are
 // different than the error messages provided by the SDK.
 export default function enhanceMessage(message = '', code?: number) {
-  switch (code) {
-    case 20101: // Invalid token error
+  switch (true) {
+    case code === 20101: // Invalid token error
       return message + '. Please make sure you are using the correct credentials.';
+    case message === 'Permission denied by system':
+      return 'Unable to share your screen. Please make sure that your operating system has the correct permissions enabled for screen sharing.';
     default:
       return message;
   }
