42ffe6350dd4d20045fed03b106113a504576176
mmalavalli <mmalavalli@twilio.com>
2022-06-23 17:41:30 -0700

    Adding URL parameters to pass new ConnectOptions properties

--- src/utils/useConnectionOptions/useConnectionOptions.ts
+++ src/utils/useConnectionOptions/useConnectionOptions.ts
@@ -4,6 +4,7 @@ import { useAppState } from '../../state';
 
 export default function useConnectionOptions() {
   const { settings } = useAppState();
+  const params = new URLSearchParams(window.location.search);
 
   // See: https://sdk.twilio.com/js/video/releases/2.0.0/docs/global.html#ConnectOptions
   // for available connection options.
@@ -31,6 +32,15 @@ export default function useConnectionOptions() {
 
     //@ts-ignore - Internal use only. This property is not exposed in type definitions.
     environment: process.env.REACT_APP_TWILIO_ENVIRONMENT,
+
+    //@ts-ignore - Internal use only.
+    screenShareEncodingPriority: params.get('screenShareEncodingPriority') || 'low',
+
+    //@ts-ignore - Internal use only.
+    screenShareContentHint: params.get('screenShareContentHint') || '',
+
+    //@ts-ignore - Internal use only.
+    singleScreenShareActiveLayer: JSON.parse(params.get('singleScreenShareActiveLayer') || 'false'),
   };
 
   // For mobile browsers, limit the maximum incoming video bitrate to 2.5 Mbps.
