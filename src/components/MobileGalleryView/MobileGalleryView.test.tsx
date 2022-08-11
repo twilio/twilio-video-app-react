9bcaad75a106b1773ac73de2cc593076624cbce3
timmydoza <tmendoza@twilio.com>
2022-08-11 16:44:56 -0600

    Fix mobile landscape grid (#738)
    * Get storybook working with react-scripts 5
    
    * Fix landscape behavior in MobileGalleryView component
    
    * Update existing tests and add tests for landscape MobileGalleryView
    
    * fix storybook and npm start command

--- src/components/MobileGalleryView/MobileGalleryView.test.tsx
+++ src/components/MobileGalleryView/MobileGalleryView.test.tsx
@@ -1,6 +1,7 @@
 import { MobileGalleryView } from './MobileGalleryView';
 import { shallow } from 'enzyme';
 import { useAppState } from '../../state';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import useParticipantContext from '../../hooks/useParticipantsContext/useParticipantsContext';
 import useVideoContext from '../../hooks/useVideoContext/useVideoContext';
 
@@ -18,10 +19,12 @@ jest.mock('swiper', () => ({
 jest.mock('../../hooks/useParticipantsContext/useParticipantsContext');
 jest.mock('../../hooks/useVideoContext/useVideoContext');
 jest.mock('../../state');
+jest.mock('@material-ui/core/useMediaQuery');
 
 const mockUseVideoContext = useVideoContext as jest.Mock<any>;
 const mockUseParticipantContext = useParticipantContext as jest.Mock<any>;
 const mockUseAppState = useAppState as jest.Mock<any>;
+const mockUseMediaQuery = useMediaQuery as jest.Mock<any>;
 
 mockUseAppState.mockImplementation(() => ({ maxGalleryViewParticipants: 9 }));
 mockUseVideoContext.mockImplementation(() => ({
@@ -34,55 +37,179 @@ mockUseParticipantContext.mockImplementation(() => ({
 }));
 
 describe('the MobileGalleryView component', () => {
-  it('should render correctly when there is only the localParticipant', () => {
-    const wrapper = shallow(<MobileGalleryView />);
-    expect(wrapper).toMatchSnapshot();
+  describe('in portrait orientation', () => {
+    beforeEach(() => {
+      mockUseMediaQuery.mockImplementation(() => false);
+    });
+
+    it('should render correctly when there is only the localParticipant', () => {
+      mockUseParticipantContext.mockImplementation(() => ({
+        mobileGalleryViewParticipants: [],
+      }));
+
+      const wrapper = shallow(<MobileGalleryView />);
+      const containers = wrapper.find('[data-test-id="participantContainer"]');
+      expect(containers.length).toBe(1);
+      expect(containers.prop('style')).toMatchObject({
+        height: '100%',
+        width: '100%',
+      });
+    });
+
+    it('should render correctly when there is one participant plus the localParticipant', () => {
+      mockUseParticipantContext.mockImplementation(() => ({
+        mobileGalleryViewParticipants: [{ identity: 'test-participant-1', sid: 1 }],
+      }));
+
+      const wrapper = shallow(<MobileGalleryView />);
+      const containers = wrapper.find('[data-test-id="participantContainer"]');
+      expect(containers.length).toBe(2);
+      expect(containers.at(0).prop('style')).toMatchObject({
+        height: '50%',
+        width: '100%',
+      });
+    });
+
+    it('should render correctly when there are two participants plus the localParticipant', () => {
+      mockUseParticipantContext.mockImplementation(() => ({
+        mobileGalleryViewParticipants: [
+          { identity: 'test-participant-1', sid: 1 },
+          { identity: 'test-participant-2', sid: 2 },
+        ],
+      }));
+
+      const wrapper = shallow(<MobileGalleryView />);
+      const containers = wrapper.find('[data-test-id="participantContainer"]');
+      expect(containers.length).toBe(3);
+      expect(containers.at(0).prop('style')).toMatchObject({
+        height: '33.33%',
+        width: '100%',
+      });
+    });
+
+    it('should render correctly when there are 3 participants plus the localParticipant', () => {
+      mockUseParticipantContext.mockImplementation(() => ({
+        mobileGalleryViewParticipants: [
+          { identity: 'test-participant-1', sid: 1 },
+          { identity: 'test-participant-2', sid: 2 },
+          { identity: 'test-participant-3', sid: 3 },
+        ],
+      }));
+
+      const wrapper = shallow(<MobileGalleryView />);
+      const containers = wrapper.find('[data-test-id="participantContainer"]');
+      expect(containers.length).toBe(4);
+      expect(containers.at(0).prop('style')).toMatchObject({
+        height: '50%',
+        width: '50%',
+      });
+    });
+
+    it('should render correctly when there are 3 or more participants plus the localParticipant', () => {
+      mockUseParticipantContext.mockImplementation(() => ({
+        mobileGalleryViewParticipants: [
+          { identity: 'test-participant-1', sid: 1 },
+          { identity: 'test-participant-2', sid: 2 },
+          { identity: 'test-participant-3', sid: 3 },
+          { identity: 'test-participant-4', sid: 4 },
+        ],
+      }));
+
+      const wrapper = shallow(<MobileGalleryView />);
+      const containers = wrapper.find('[data-test-id="participantContainer"]');
+      expect(containers.length).toBe(5);
+      expect(containers.at(0).prop('style')).toMatchObject({
+        height: '33.33%',
+        width: '50%',
+      });
+    });
   });
 
-  it('should render correctly when there is one participant plus the localParticipant', () => {
-    mockUseVideoContext.mockImplementation(() => ({
-      room: {
-        localParticipant: mockLocalParticipant,
-      },
-    }));
-    mockUseParticipantContext.mockImplementation(() => ({
-      mobileGalleryViewParticipants: [{ identity: 'test-participant-1', sid: 1 }],
-    }));
-    const wrapper = shallow(<MobileGalleryView />);
-    expect(wrapper).toMatchSnapshot();
-  });
+  describe('in landscape orientation', () => {
+    beforeEach(() => {
+      mockUseMediaQuery.mockImplementation(() => true);
+    });
 
-  it('should render correctly when there are two participants plus the localParticipant', () => {
-    mockUseVideoContext.mockImplementation(() => ({
-      room: {
-        localParticipant: mockLocalParticipant,
-      },
-    }));
-    mockUseParticipantContext.mockImplementation(() => ({
-      mobileGalleryViewParticipants: [
-        { identity: 'test-participant-1', sid: 1 },
-        { identity: 'test-participant-2', sid: 2 },
-      ],
-    }));
-    const wrapper = shallow(<MobileGalleryView />);
-    expect(wrapper).toMatchSnapshot();
-  });
+    it('should render correctly when there is only the localParticipant', () => {
+      mockUseParticipantContext.mockImplementation(() => ({
+        mobileGalleryViewParticipants: [],
+      }));
 
-  it('should render correctly when there are 3 or more participants plus the localParticipant', () => {
-    mockUseVideoContext.mockImplementation(() => ({
-      room: {
-        localParticipant: mockLocalParticipant,
-      },
-    }));
-    mockUseParticipantContext.mockImplementation(() => ({
-      mobileGalleryViewParticipants: [
-        { identity: 'test-participant-1', sid: 1 },
-        { identity: 'test-participant-2', sid: 2 },
-        { identity: 'test-participant-3', sid: 3 },
-        { identity: 'test-participant-4', sid: 4 },
-      ],
-    }));
-    const wrapper = shallow(<MobileGalleryView />);
-    expect(wrapper).toMatchSnapshot();
+      const wrapper = shallow(<MobileGalleryView />);
+      const containers = wrapper.find('[data-test-id="participantContainer"]');
+      expect(containers.length).toBe(1);
+      expect(containers.at(0).prop('style')).toMatchObject({
+        height: '100%',
+        width: '100%',
+      });
+    });
+
+    it('should render correctly when there is one participant plus the localParticipant', () => {
+      mockUseParticipantContext.mockImplementation(() => ({
+        mobileGalleryViewParticipants: [{ identity: 'test-participant-1', sid: 1 }],
+      }));
+
+      const wrapper = shallow(<MobileGalleryView />);
+      const containers = wrapper.find('[data-test-id="participantContainer"]');
+      expect(containers.length).toBe(2);
+      expect(containers.at(0).prop('style')).toMatchObject({
+        height: '100%',
+        width: '50%',
+      });
+    });
+
+    it('should render correctly when there are two participants plus the localParticipant', () => {
+      mockUseParticipantContext.mockImplementation(() => ({
+        mobileGalleryViewParticipants: [
+          { identity: 'test-participant-1', sid: 1 },
+          { identity: 'test-participant-2', sid: 2 },
+        ],
+      }));
+
+      const wrapper = shallow(<MobileGalleryView />);
+      const containers = wrapper.find('[data-test-id="participantContainer"]');
+      expect(containers.length).toBe(3);
+      expect(containers.at(0).prop('style')).toMatchObject({
+        height: '100%',
+        width: '33.33%',
+      });
+    });
+
+    it('should render correctly when there are 3 participants plus the localParticipant', () => {
+      mockUseParticipantContext.mockImplementation(() => ({
+        mobileGalleryViewParticipants: [
+          { identity: 'test-participant-1', sid: 1 },
+          { identity: 'test-participant-2', sid: 2 },
+          { identity: 'test-participant-3', sid: 3 },
+        ],
+      }));
+
+      const wrapper = shallow(<MobileGalleryView />);
+      const containers = wrapper.find('[data-test-id="participantContainer"]');
+      expect(containers.length).toBe(4);
+      expect(containers.at(0).prop('style')).toMatchObject({
+        height: '50%',
+        width: '50%',
+      });
+    });
+
+    it('should render correctly when there are 3 or more participants plus the localParticipant', () => {
+      mockUseParticipantContext.mockImplementation(() => ({
+        mobileGalleryViewParticipants: [
+          { identity: 'test-participant-1', sid: 1 },
+          { identity: 'test-participant-2', sid: 2 },
+          { identity: 'test-participant-3', sid: 3 },
+          { identity: 'test-participant-4', sid: 4 },
+        ],
+      }));
+
+      const wrapper = shallow(<MobileGalleryView />);
+      const containers = wrapper.find('[data-test-id="participantContainer"]');
+      expect(containers.length).toBe(5);
+      expect(containers.at(0).prop('style')).toMatchObject({
+        height: '50%',
+        width: '33.33%',
+      });
+    });
   });
 });
