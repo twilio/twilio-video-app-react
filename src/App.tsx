31addaf686d2ef2d30941ae821b5f7184c6d0aaf
Luis Rivas <lrivas@twilio.com>
2025-10-14 11:02:18 -0500

    nit: comment on the why we set the Container height

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,12 @@ const Main = styled('main')(({ theme }: { theme: Theme }) => ({
 
 export default function App() {
   const roomState = useRoomState();
+
+  // Here we would like the height of the main container to be the height of the viewport.
+  // On some mobile browsers, 'height: 100vh' sets the height equal to that of the screen,
+  // not the viewport. This looks bad when the mobile browsers location bar is open.
+  // We will dynamically set the height with 'window.innerHeight', which means that this
+  // will look good on mobile browsers even after the location bar opens or closes.
   const height = useHeight();
 
   return (
